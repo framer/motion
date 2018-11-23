d53d69edc358183ed2eef56a103339deff1acb31
InventingWithMonster <mattgperry@gmail.com>
2018-11-23 08:47:06 +0000

    test

--- src/motion/__tests__/component.test.tsx
+++ src/motion/__tests__/component.test.tsx
@@ -1,26 +1,33 @@
 import { render } from 'react-testing-library';
 import motion from '../';
 import * as React from 'react';
+import useMotionValue from '../../hooks/use-motion-value';
+import styled from 'styled-components';
 
-//import useMotionValue from '../../hooks/use-motion-value';
-//
-//import styled from 'styled-components';
+/**
+ * Note:
+ *
+ * There's currently a "bug" inherent in testing components using React hooks.
+ * Components aren't really mounted/rendered with the initial render and it
+ * requires a `rerender` to force this behaviour. The React team are working
+ * to offer a way to flush effects but until then we have these rerenders in async tests.
+ */
 
-// test('motion component renders', () => {
-//   const Box = motion.div();
-//   const { container } = render(<Box />);
-//   expect(container.firstChild).toBeTruthy();
-// });
+test('motion component renders', () => {
+  const Box = motion.div();
+  const { container } = render(<Box />);
+  expect(container.firstChild).toBeTruthy();
+});
 
-// test('motion component renders custom component', () => {
-//   const Box = motion.div();
-//   const { getByTestId } = render(
-//     <Box>
-//       <div data-testid="child" />
-//     </Box>
-//   );
-//   expect(getByTestId('child')).toBeTruthy();
-// });
+test('motion component renders custom component', () => {
+  const Box = motion.div();
+  const { getByTestId } = render(
+    <Box>
+      <div data-testid="child" />
+    </Box>
+  );
+  expect(getByTestId('child')).toBeTruthy();
+});
 
 test('motion component accepts function ref', async () => {
   const Box = motion.div();
@@ -34,97 +41,114 @@ test('motion component accepts function ref', async () => {
   await expect(promise).resolves.toBeTruthy();
 });
 
-// test('motion component accepts createRef', () => {
-//   const Box = motion.div();
+test('motion component accepts createRef', async () => {
+  const Box = motion.div();
 
-//   const { container } = render(<Box />);
-//   expect(container.firstChild).toBeTruthy();
-// });
+  const promise = new Promise<null | HTMLElement>(resolve => {
+    const ref = React.createRef<null | HTMLElement>();
+    const Component = () => {
+      React.useEffect(() => {
+        resolve(ref.current);
+      });
+      return <Box ref={ref} />;
+    };
+    const { rerender } = render(<Component />);
+    rerender(<Component />);
+  });
 
-// test('motion component generates style attr from pose', () => {
-//   const Box = motion.div({
-//     default: { backgroundColor: '#fff' }
-//   });
-//   const { container } = render(<Box />);
-//   expect(container.firstChild).toHaveStyle('background-color: #fff');
-// });
+  await expect(promise).resolves.toBeTruthy();
+});
 
-// test('motion component overwrites provided style attr with pose', () => {
-//   const Box = motion.div({
-//     default: { backgroundColor: '#fff' }
-//   });
-//   const { container } = render(
-//     <Box style={{ backgroundColor: '#000', left: 500 }} />
-//   );
-//   expect(container.firstChild).toHaveStyle(
-//     'background-color: #fff; left: 500px'
-//   );
-// });
+test('motion component generates style attr from pose', () => {
+  const Box = motion.div({
+    default: { backgroundColor: '#fff' }
+  });
+  const { container } = render(<Box />);
+  expect(container.firstChild).toHaveStyle('background-color: #fff');
+});
 
-// test('motion component renders styled component and overwrites style', () => {
-//   const Box = styled.div`
-//     background: #fff;
-//   `;
-//   const MotionBox = motion(Box)({
-//     default: { background: '#000' }
-//   });
+test('motion component overwrites provided style attr with pose', () => {
+  const Box = motion.div({
+    default: { backgroundColor: '#fff' }
+  });
+  const { container } = render(
+    <Box style={{ backgroundColor: '#000', left: 500 }} />
+  );
+  expect(container.firstChild).toHaveStyle(
+    'background-color: #fff; left: 500px'
+  );
+});
 
-//   const { container } = render(<MotionBox style={{ background: 'red' }} />);
+test('motion component renders styled component and overwrites style', () => {
+  const Box = styled.div`
+    background: #fff;
+  `;
+  const MotionBox = motion(Box)({
+    default: { background: '#000' }
+  });
 
-//   expect(container.firstChild).toHaveStyle('background: #000');
-// });
+  const { container } = render(<MotionBox style={{ background: 'red' }} />);
 
-// test('motion component takes styles of defined initial pose', () => {
-//   const Box = motion.div({
-//     default: { x: 0 },
-//     foo: { x: 100, transitionEnd: { display: 'none' } }
-//   });
-//   const { container } = render(<Box pose="foo" />);
-//   expect(container.firstChild).toHaveStyle(
-//     'transform: translateX(100px) translateZ(0); display: none;'
-//   );
-// });
+  expect(container.firstChild).toHaveStyle('background: #000');
+});
 
-// test("motion component doesn't forward pose prop", () => {
-//   const Box = motion.div({ default: {} });
-//   const { container } = render(<Box pose="default" />);
-//   expect(container.firstChild).not.toHaveAttribute('pose');
-// });
+test('motion component takes styles of defined initial pose', () => {
+  const Box = motion.div({
+    default: { x: 0 },
+    foo: { x: 100, transitionEnd: { display: 'none' } }
+  });
+  const { container } = render(<Box pose="foo" />);
+  expect(container.firstChild).toHaveStyle(
+    'transform: translateX(100px) translateZ(0); display: none;'
+  );
+});
 
-// test('motion component accepts motion value', () => {
-//   const Box = motion.div();
+test("motion component doesn't forward pose prop", () => {
+  const Box = motion.div({ default: {} });
+  const { container } = render(<Box pose="default" />);
+  expect(container.firstChild).not.toHaveAttribute('pose');
+});
 
-//   const Component = () => {
-//     const x = useMotionValue(800);
-//     return <Box x={x} />;
-//   };
+test('motion component accepts motion value', () => {
+  const Box = motion.div();
 
-//   const { container } = render(<Component />);
-//   expect(container.firstChild).toHaveStyle(
-//     'transform: translateX(800px) translateZ(0)'
-//   );
-// });
+  const Component = () => {
+    const x = useMotionValue(800);
+    return <Box x={x} />;
+  };
 
-// test('motion component fires onPoseComplete', done => {
-//   const Box = motion.div({
-//     foo: { x: 100 },
-//     bar: { x: 0, transition: false }
-//   });
+  const { container } = render(<Component />);
+  expect(container.firstChild).toHaveStyle(
+    'transform: translateX(800px) translateZ(0)'
+  );
+});
 
-//   const { rerender } = render(<Box pose="foo" />);
+test('motion component fires onPoseComplete', async () => {
+  const Box = motion.div({
+    foo: { x: 100 },
+    bar: { x: 0, transition: false }
+  });
 
-//   rerender(
-//     <Box
-//       pose="bar"
-//       onPoseComplete={current => {
-//         // We would ideally like to inspect the DOM itself at this point
-//         // but the node seems to be unmounted after sync ops have flushed
-//         expect(current.x).toBe(0);
-//         done();
-//       }}
-//     />
-//   );
-// });
+  const promise = new Promise(resolve => {
+    const { rerender } = render(<Box pose="foo" />);
+    rerender(
+      <Box
+        pose="bar"
+        onPoseComplete={current => {
+          // We would ideally like to inspect the DOM itself at this point
+          // but the node seems to be unmounted after sync ops have flushed
+          expect(current.x).toBe(0);
+          resolve('fired');
+        }}
+      />
+    );
+
+    // To fire effects
+    rerender(<Box pose="bar" />);
+  });
+
+  await expect(promise).resolves.toEqual('fired');
+});
 
 // usePose
 
