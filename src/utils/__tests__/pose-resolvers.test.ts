4dbe69cedf6158186a65f0686052d1bcb67acffa
Niels van Hoorn <nvh@nvh.io>
2018-11-28 12:01:16 +0100

    Fix pose-resolver tests

--- src/utils/__tests__/pose-resolvers.test.ts
+++ src/utils/__tests__/pose-resolvers.test.ts
@@ -1,15 +1,15 @@
 import { poseToArray } from "../pose-resolvers"
 
 describe("poseToArray", () => {
-    it("Should correctly convert single string to array", () => {
-        expect(poseToArray("a")).toEqual(["a"])
+    it("Should correctly convert single string to array with the default pose applied", () => {
+        expect(poseToArray("a")).toEqual(["default", "a"])
     })
 
     it("Should leave arrays as-is", () => {
-        expect(poseToArray(["a", "b"])).toEqual(["a", "b"])
+        expect(poseToArray(["a", "b"])).toEqual(["default", "a", "b"])
     })
 
     it("Should remove falsey poses", () => {
-        expect(poseToArray()).toEqual([])
+        expect(poseToArray()).toEqual(["default"])
     })
 })
--- src/utils/pose-resolvers.ts
+++ src/utils/pose-resolvers.ts
@@ -5,7 +5,7 @@ type PoseName = string | PoseNameList
 type UnresolvedPose = PoseName | MotionValue
 
 export const poseToArray = (pose?: PoseName): PoseNameList =>
-    Array.isArray(pose) ? ["default", ...pose] : pose ? ["default", pose] : []
+    Array.isArray(pose) ? ["default", ...pose] : pose ? ["default", pose] : ["default"]
 
 export const resolvePoses = (pose: UnresolvedPose): PoseNameList => {
     const unresolvedPose = pose instanceof MotionValue ? (pose.get() as string) : pose
