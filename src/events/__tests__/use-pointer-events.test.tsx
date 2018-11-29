05f64a5249cf97f0c32e04ff16199e5112e752dc
Niels van Hoorn <nvh@nvh.io>
2018-11-29 23:32:47 +0100

    Added use-pointer-events tests

---
+++ src/events/__tests__/use-pointer-events.test.tsx
@@ -0,0 +1,79 @@
+import "../../../jest.setup"
+import { fireEvent, render } from "react-testing-library"
+import * as React from "react"
+import { useRef } from "react"
+import { usePointerEvents } from "../"
+import { PointerEventHandlers } from "../use-pointer-events"
+import { enableTouch, enablePointer } from "./utils/event-helpers"
+import { fireCustomEvent } from "./utils/fire-event"
+
+function testEvents(name: string, fireFunction: (element: Element) => boolean) {
+    const handler = jest.fn()
+    const handlers: Partial<PointerEventHandlers> = {}
+    handlers[name] = handler
+    const Component = () => {
+        const ref = useRef(null)
+        usePointerEvents(handlers, ref)
+        return <div ref={ref} />
+    }
+    const { container, rerender } = render(<Component />)
+    rerender(<Component />)
+    fireFunction(container.firstChild as Element)
+    expect(handler).toHaveBeenCalledTimes(1)
+}
+
+const touchHandlers = {
+    onPointerDown: fireEvent.touchStart,
+    onPointerMove: fireEvent.touchMove,
+    onPointerUp: fireEvent.touchEnd,
+    onPointerCancel: fireEvent.touchCancel,
+}
+
+const mouseHandlers = {
+    onPointerDown: fireEvent.mouseDown,
+    onPointerMove: fireEvent.mouseMove,
+    onPointerUp: fireEvent.mouseUp,
+    onPointerOver: fireEvent.mouseOver,
+    onPointerOut: fireEvent.mouseOut,
+    onPointerEnter: fireEvent.mouseEnter,
+    onPointerLeave: fireEvent.mouseLeave,
+}
+
+const pointerHandlers = {
+    onPointerDown: fireCustomEvent("pointerdown"),
+    onPointerMove: fireCustomEvent("pointermove"),
+    onPointerUp: fireCustomEvent("pointerup"),
+    onPointerCancel: fireCustomEvent("pointercancel"),
+    onPointerOver: fireCustomEvent("pointerover"),
+    onPointerOut: fireCustomEvent("pointerout"),
+    onPointerEnter: fireCustomEvent("pointerenter"),
+    onPointerLeave: fireCustomEvent("pointerleave"),
+}
+
+describe("usePointerEvents", () => {
+    describe("on touch devices", () => {
+        for (const key in touchHandlers) {
+            it(`should call ${key} handler`, () => {
+                const restore = enableTouch()
+                testEvents(key, touchHandlers[key])
+                restore()
+            })
+        }
+    })
+    describe("on mouse devices", () => {
+        for (const key in mouseHandlers) {
+            it(`should call ${key} handler`, () => {
+                testEvents(key, mouseHandlers[key])
+            })
+        }
+    })
+    describe.skip("on pointer devices", () => {
+        for (const key in pointerHandlers) {
+            it.only(`should call ${key} handler`, () => {
+                const restore = enablePointer()
+                testEvents(key, pointerHandlers[key])
+                restore()
+            })
+        }
+    })
+})
---
+++ src/events/__tests__/utils/event-helpers.ts
@@ -0,0 +1,31 @@
+export const enableTouch = () => {
+    const originalEvents = {
+        ontouchstart: window.ontouchstart,
+        ontouchmove: window.ontouchmove,
+        ontouchend: window.ontouchend,
+    }
+    window.ontouchstart = null
+    window.ontouchmove = null
+    window.ontouchend = null
+    return () => {
+        window.ontouchstart = originalEvents.ontouchstart
+        window.ontouchmove = originalEvents.ontouchmove
+        window.ontouchend = originalEvents.ontouchend
+    }
+}
+
+export const enablePointer = () => {
+    const originalEvents = {
+        onpointerdown: window.onpointerdown,
+        onpointermove: window.onpointermove,
+        onpointerup: window.onpointerup,
+    }
+    window.onpointerdown = null
+    window.onpointermove = null
+    window.onpointerup = null
+    return () => {
+        window.onpointerdown = originalEvents.onpointerdown
+        window.onpointermove = originalEvents.onpointermove
+        window.onpointerup = originalEvents.onpointerup
+    }
+}
