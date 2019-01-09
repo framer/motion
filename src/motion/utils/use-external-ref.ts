0c594f548e9c3c9c83b9da130d67da280133daa3
Niels van Hoorn <nvh@nvh.io>
2019-01-09 16:47:38 +0100

    made return type of useExternalRef explicit
    to better signify the use of this hook

--- src/motion/utils/use-external-ref.ts
+++ src/motion/utils/use-external-ref.ts
@@ -1,6 +1,6 @@
-import { useEffect, useRef, Ref, MutableRefObject } from "react"
+import { useEffect, useRef, Ref, MutableRefObject, RefObject } from "react"
 
-export const useExternalRef = (external?: Ref<Element | null>) => {
+export const useExternalRef = (external?: Ref<Element | null>): RefObject<Element | null> => {
     const ref = !external || typeof external === "function" ? useRef(null) : external
 
     useEffect(() => {
