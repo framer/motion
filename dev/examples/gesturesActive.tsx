d4f7ff58e1b42fd43525835f290320a3480c933a
Niels van Hoorn <nvh@nvh.io>
2019-01-29 16:57:13 +0100

    Updated example

--- dev/examples/gesturesActive.tsx
+++ dev/examples/gesturesActive.tsx
@@ -14,6 +14,7 @@ export const App = () => {
         <motion.div
             animate={animation}
             hoverActive={{ scale: 1.5 }}
+            tapActive={{ scale: 0.5, backgroundColor: "rgba(0, 255, 0, .5)" }}
             onHoverStart={() => {
                 animation.start({ background: "#00F" })
             }}
