e461020af234e42dc08fd402db26147eddea6937
InventingWithMonster <mattgperry@gmail.com>
2019-02-08 09:36:27 +0000

    adding comment

--- src/motion/utils/MotionPluginContext.tsx
+++ src/motion/utils/MotionPluginContext.tsx
@@ -12,6 +12,8 @@ export interface MotionPluginProps extends MotionPlugins {
 
 export const MotionPluginContext = createContext<Partial<MotionPlugins>>({})
 
+// For now I think this should remain a private API for our own use until
+// we can figure out a nicer way of allowing people to add these
 export const MotionPlugins = ({ children, ...props }: MotionPluginProps) => {
     const pluginContext = useContext(MotionPluginContext)
     const value = useRef({ ...pluginContext }).current
