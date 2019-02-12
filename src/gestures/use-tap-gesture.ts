3d34e63873185fe3df3c4e342edd5867a20c8f7b
Matt Perry <mattgperry@gmail.com>
2019-02-12 08:27:12 +0000

    Fixing SSR (#31)
    * adding tests
    
    * combining test scripts
    
    * fixed types
    
    * reducing type complexityu

--- src/gestures/use-tap-gesture.ts
+++ src/gestures/use-tap-gesture.ts
@@ -9,6 +9,7 @@ import {
 import { TargetAndTransition } from "../types"
 import { getGesturePriority } from "./utils/gesture-priority"
 import { ControlsProp } from "./types"
+import { safeWindow } from "../events/utils/window"
 
 interface TapInfo {
     point: Point
@@ -136,7 +137,7 @@ export function useTapGesture(
 
     const [startPointerUp, stopPointerUp] = usePointerEvents(
         { onPointerUp: handlers.onPointerUp },
-        window
+        safeWindow
     )
 
     useEffect(
