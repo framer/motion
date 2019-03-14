17d9ead4c2d8820d4ea4200899f60ca2edbc00c1
Artem <asci@yandex.ru>
2019-03-14 16:50:00 +0100

    update example

--- dev/examples/pointRelativeTo.tsx
+++ dev/examples/pointRelativeTo.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { useState } from "react"
-import { motion, useTransformedValue, useMotionValue } from "@framer"
+import { motion, Point } from "../../src"
 import styled from "styled-components"
 
 const Slider = styled(motion.div)`
@@ -11,37 +11,21 @@ const Slider = styled(motion.div)`
     width: 400px;
 `
 
-function pointRelativeTo(id: string | HTMLElement) {
-    let elem: HTMLElement
-
-    function getElem() {
-        if (elem) return elem
-        if (typeof id === "string") {
-            elem = document.getElementById(id)
-        } else {
-            elem = id
-        }
-    }
-    return ({ x, y }) => {
-        const localElem = getElem()
-
-        if (!localElem) return undefined
-        const rect = localElem.getBoundingClientRect()
-
-        return {
-            x: x - rect.left - localElem.scrollLeft,
-            y: y - rect.top - localElem.scrollTop,
-        }
-    }
-}
-const convert = pointRelativeTo("slider")
+const convert1 = Point.pointRelativeTo("slider1")
+const convert2 = Point.pointRelativeTo("slider2")
 
 export const App = () => {
     const [point, setPoint] = useState({ x: 0, y: 0 })
     return (
         <div style={{ position: "absolute", left: 100, top: 204 }}>
-            <Slider onPan={(e, info) => setPoint(info.point)} id="slider">
-                {JSON.stringify(convert(point))}
+            <Slider onPan={(e, info) => setPoint(info.point)} id="slider1">
+                {JSON.stringify(convert1(point))}
+            </Slider>
+            <div
+                style={{ height: 20000, width: 300, backgroundColor: "tomato" }}
+            />
+            <Slider onPan={(e, info) => setPoint(info.point)} id="slider2">
+                {JSON.stringify(convert2(point))}
             </Slider>
         </div>
     )
