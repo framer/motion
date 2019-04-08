28d14c86c651dd5292e171f6d13f372a29f8be53
InventingWithMonster <mattgperry@gmail.com>
2019-04-08 16:09:59 +0100

    variants

--- src/motion/__tests__/variant.test.tsx
+++ src/motion/__tests__/variant.test.tsx
@@ -40,170 +40,170 @@ describe("animate prop as variant", () => {
         return expect(promise).resolves.toBe(100)
     })
 
-    // test("child animates to set variant", async () => {
-    //     const promise = new Promise(resolve => {
-    //         const x = motionValue(0)
-    //         const onComplete = () => resolve(x.get())
-    //         const Component = () => (
-    //             <motion.div
-    //                 animate="visible"
-    //                 variants={variants}
-    //                 onAnimationComplete={onComplete}
-    //             >
-    //                 <motion.div variants={childVariants} style={{ x }} />
-    //             </motion.div>
-    //         )
+    test("child animates to set variant", async () => {
+        const promise = new Promise(resolve => {
+            const x = motionValue(0)
+            const onComplete = () => resolve(x.get())
+            const Component = () => (
+                <motion.div
+                    animate="visible"
+                    variants={variants}
+                    onAnimationComplete={onComplete}
+                >
+                    <motion.div variants={childVariants} style={{ x }} />
+                </motion.div>
+            )
 
-    //         const { rerender } = render(<Component />)
-    //         rerender(<Component />)
-    //     })
+            const { rerender } = render(<Component />)
+            rerender(<Component />)
+        })
 
-    //     return expect(promise).resolves.toBe(50)
-    // })
+        return expect(promise).resolves.toBe(50)
+    })
 
-    // test("child animates to set variant even if variants are not found on parent", async () => {
-    //     const promise = new Promise(resolve => {
-    //         const x = motionValue(0)
-    //         const onComplete = () => resolve(x.get())
-    //         const Component = () => (
-    //             <motion.div animate="visible" onAnimationComplete={onComplete}>
-    //                 <motion.div variants={childVariants} style={{ x }} />
-    //             </motion.div>
-    //         )
+    test("child animates to set variant even if variants are not found on parent", async () => {
+        const promise = new Promise(resolve => {
+            const x = motionValue(0)
+            const onComplete = () => resolve(x.get())
+            const Component = () => (
+                <motion.div animate="visible" onAnimationComplete={onComplete}>
+                    <motion.div variants={childVariants} style={{ x }} />
+                </motion.div>
+            )
 
-    //         const { rerender } = render(<Component />)
-    //         rerender(<Component />)
-    //     })
+            const { rerender } = render(<Component />)
+            rerender(<Component />)
+        })
 
-    //     return expect(promise).resolves.toBe(50)
-    // })
+        return expect(promise).resolves.toBe(50)
+    })
 
-    // test("applies applyOnEnd if set on initial", () => {
-    //     const variants: Variants = {
-    //         visible: {
-    //             background: "#f00",
-    //             transitionEnd: { display: "none" },
-    //         },
-    //     }
+    test("applies applyOnEnd if set on initial", () => {
+        const variants: Variants = {
+            visible: {
+                background: "#f00",
+                transitionEnd: { display: "none" },
+            },
+        }
 
-    //     const { container } = render(
-    //         <motion.div variants={variants} initial="visible" />
-    //     )
-    //     expect(container.firstChild).toHaveStyle("display: none")
-    // })
+        const { container } = render(
+            <motion.div variants={variants} initial="visible" />
+        )
+        expect(container.firstChild).toHaveStyle("display: none")
+    })
 
-    // test("applies applyOnEnd and end of animation", async () => {
-    //     const promise = new Promise(resolve => {
-    //         const variants: Variants = {
-    //             hidden: { background: "#00f" },
-    //             visible: {
-    //                 background: "#f00",
-    //                 transitionEnd: { display: "none" },
-    //             },
-    //         }
-    //         const display = motionValue("block")
-    //         const onComplete = () => resolve(display.get())
-    //         const Component = () => (
-    //             <motion.div
-    //                 initial="hidden"
-    //                 animate="visible"
-    //                 variants={variants}
-    //                 transition={{ type: false }}
-    //                 onAnimationComplete={onComplete}
-    //                 style={{ display }}
-    //             />
-    //         )
+    test("applies applyOnEnd and end of animation", async () => {
+        const promise = new Promise(resolve => {
+            const variants: Variants = {
+                hidden: { background: "#00f" },
+                visible: {
+                    background: "#f00",
+                    transitionEnd: { display: "none" },
+                },
+            }
+            const display = motionValue("block")
+            const onComplete = () => resolve(display.get())
+            const Component = () => (
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={variants}
+                    transition={{ type: false }}
+                    onAnimationComplete={onComplete}
+                    style={{ display }}
+                />
+            )
 
-    //         const { rerender } = render(<Component />)
-    //         rerender(<Component />)
-    //     })
+            const { rerender } = render(<Component />)
+            rerender(<Component />)
+        })
 
-    //     return expect(promise).resolves.toBe("none")
-    // })
+        return expect(promise).resolves.toBe("none")
+    })
 
-    // test("accepts custom transition", async () => {
-    //     const promise = new Promise(resolve => {
-    //         const variants: Variants = {
-    //             hidden: { background: "#00f" },
-    //             visible: {
-    //                 background: "#f00",
-    //                 transition: { to: "#555" },
-    //             },
-    //         }
-    //         const background = motionValue("#00f")
-    //         const onComplete = () => resolve(background.get())
-    //         const Component = () => (
-    //             <motion.div
-    //                 initial="hidden"
-    //                 animate="visible"
-    //                 variants={variants}
-    //                 transition={{ type: false }}
-    //                 onAnimationComplete={onComplete}
-    //                 style={{ background }}
-    //             />
-    //         )
+    test("accepts custom transition", async () => {
+        const promise = new Promise(resolve => {
+            const variants: Variants = {
+                hidden: { background: "#00f" },
+                visible: {
+                    background: "#f00",
+                    transition: { to: "#555" },
+                },
+            }
+            const background = motionValue("#00f")
+            const onComplete = () => resolve(background.get())
+            const Component = () => (
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={variants}
+                    transition={{ type: false }}
+                    onAnimationComplete={onComplete}
+                    style={{ background }}
+                />
+            )
 
-    //         const { rerender } = render(<Component />)
-    //         rerender(<Component />)
-    //     })
+            const { rerender } = render(<Component />)
+            rerender(<Component />)
+        })
 
-    //     return expect(promise).resolves.toBe("rgba(85, 85, 85, 1)")
-    // })
+        return expect(promise).resolves.toBe("rgba(85, 85, 85, 1)")
+    })
 
-    // test("respects orchestration props in transition prop", async () => {
-    //     const promise = new Promise(resolve => {
-    //         const opacity = motionValue(0)
-    //         const variants: Variants = {
-    //             visible: {
-    //                 opacity: 1,
-    //             },
-    //             hidden: {
-    //                 opacity: 0,
-    //             },
-    //         }
+    test("respects orchestration props in transition prop", async () => {
+        const promise = new Promise(resolve => {
+            const opacity = motionValue(0)
+            const variants: Variants = {
+                visible: {
+                    opacity: 1,
+                },
+                hidden: {
+                    opacity: 0,
+                },
+            }
 
-    //         render(
-    //             <motion.div
-    //                 variants={variants}
-    //                 initial="hidden"
-    //                 animate="visible"
-    //                 transition={{ type: false, delayChildren: 1 }}
-    //             >
-    //                 <motion.div
-    //                     variants={variants}
-    //                     transition={{ type: false }}
-    //                     style={{ opacity }}
-    //                 />
-    //             </motion.div>
-    //         )
+            render(
+                <motion.div
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ type: false, delayChildren: 1 }}
+                >
+                    <motion.div
+                        variants={variants}
+                        transition={{ type: false }}
+                        style={{ opacity }}
+                    />
+                </motion.div>
+            )
 
-    //         requestAnimationFrame(() => resolve(opacity.get()))
-    //     })
+            requestAnimationFrame(() => resolve(opacity.get()))
+        })
 
-    //     return expect(promise).resolves.toBe(0)
-    // })
+        return expect(promise).resolves.toBe(0)
+    })
 
-    // test("onUpdate", async () => {
-    //     const promise = new Promise(resolve => {
-    //         let latest = {}
+    test("onUpdate", async () => {
+        const promise = new Promise(resolve => {
+            let latest = {}
 
-    //         const onUpdate = (l: { [key: string]: number | string }) =>
-    //             (latest = l)
+            const onUpdate = (l: { [key: string]: number | string }) =>
+                (latest = l)
 
-    //         const Component = () => (
-    //             <motion.div
-    //                 onUpdate={onUpdate}
-    //                 initial={{ x: 0, y: 0 }}
-    //                 animate={{ x: 100, y: 100 }}
-    //                 transition={{ duration: 0.1 }}
-    //                 onAnimationComplete={() => resolve(latest)}
-    //             />
-    //         )
+            const Component = () => (
+                <motion.div
+                    onUpdate={onUpdate}
+                    initial={{ x: 0, y: 0 }}
+                    animate={{ x: 100, y: 100 }}
+                    transition={{ duration: 0.1 }}
+                    onAnimationComplete={() => resolve(latest)}
+                />
+            )
 
-    //         const { rerender } = render(<Component />)
-    //         rerender(<Component />)
-    //     })
+            const { rerender } = render(<Component />)
+            rerender(<Component />)
+        })
 
-    //     return expect(promise).resolves.toEqual({ x: 100, y: 100 })
-    // })
+        return expect(promise).resolves.toEqual({ x: 100, y: 100 })
+    })
 })
