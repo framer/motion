55a3c4afcb17f61f132597f616f70f227b765ba9
Benjamin den Boer <benjamin@framer.com>
2019-04-09 11:07:32 +0200

    Ranges

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -38,8 +38,6 @@ const getMixer = (v: any) => (isCustomValueType(v) ? v.mix : undefined)
  * Transforms numbers into other values by mapping them from an input range to an output range.
  * Returns a number.
  *
- * @remarks
- *
  * Given an input range of `[0, 200]` and an output range of
  * `[0, 1]`, this function will return a value between `0` and `1`.
  * The input range must be a linear series of numbers. The output range
@@ -52,19 +50,18 @@ const getMixer = (v: any) => (isCustomValueType(v) ? v.mix : undefined)
  *
  * export function MyComponent() {
  *    const rangeA = [0, 200]
- *     const rangeB = [0, 1]
- *     const output = transform(100, rangeA, rangeB)
+ *    const rangeB = [0, 1]
+ *    const output = transform(100, rangeA, rangeB)
  *
- *     return { output }
+ *    // Returns 0.5
+ *    return { output }
  * }
- *
  * ```
  *
  * @param inputValue - A number to transform between the input and output ranges.
- * @param inputRange - A linear series of numbers (either all increasing or decreasing)
+ * @param inputRange - A linear series of numbers (either all increasing or decreasing).
  * @param outputRange - A series of numbers, colors or strings. Must be the same length as `inputRange`.
  * @param options -
- *
  *  - clamp: boolean - Clamp values to within the given range. Defaults to `true`
  *
  * @public
