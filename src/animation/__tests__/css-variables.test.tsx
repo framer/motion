3da35a4026f47b433360a5791a17b4fa8d68b59c
Jonas Treub <jonas@framer.com>
2019-04-17 16:40:50 +0200

    Update test
    The test fails

--- src/animation/__tests__/css-variables.test.tsx
+++ src/animation/__tests__/css-variables.test.tsx
@@ -4,28 +4,36 @@ import { motion } from "../../motion"
 import * as React from "react"
 
 describe("css variables", () => {
-    test("has the css variable on animation end", async () => {
-        const promise = new Promise<ChildNode | null>(resolve => {
-            const resolvePromise = () => {
-                requestAnimationFrame(() => resolve(container.firstChild))
-            }
+    test("should animate css color variables", async () => {
+        const promise = new Promise(resolve => {
+            let isFirstFrame = true
+            const Component = () => (
+                <motion.div
+                    style={{ "--from": "#09F", "--to": "#F00" } as any}
+                    initial={{ background: "var(--from)" }}
+                    animate={{ background: "var(--to)" }}
+                    onUpdate={({ background }) => {
+                        console.log("update")
+                        if (isFirstFrame) {
+                            isFirstFrame = false
+                        } else {
+                            resolve(background)
+                        }
+                    }}
+                />
+            )
 
-            const Component = () => {
-                return (
-                    <motion.div
-                        style={{ "--from": "#09F", "--to": "#F00" } as any}
-                        initial={{ background: "var(--from)" }}
-                        animate={{ background: "var(--to)" }}
-                        transition={{ duration: 0.01 }}
-                        onAnimationComplete={resolvePromise}
-                    />
-                )
-            }
-
-            const { container, rerender } = render(<Component />)
+            const { rerender } = render(<Component />)
             rerender(<Component />)
         })
+        const resolvedAs = await promise
 
-        return expect(promise).resolves.toHaveStyle("background: var(--to);")
+        // We're running this on the second frame to ensure and checking that
+        // the resolved color *isn't* the start or target value. We want it to be an interpolation
+        // of the two to ensure it's actually animating and not just switching between them.
+        expect(resolvedAs).not.toBe("#09F")
+        expect(resolvedAs).not.toBe("#F00")
+        expect(resolvedAs).not.toBe("var(--from)")
+        expect(resolvedAs).not.toBe("var(--to)")
     })
 })
