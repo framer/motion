5ae28da89a70890f10a1c58bd6d94122732b7288
InventingWithMonster <mattgperry@gmail.com>
2019-05-01 15:28:44 +0100

    upgrading dependencies

--- src/motion/__tests__/unit-type-shadow.test.tsx
+++ src/motion/__tests__/unit-type-shadow.test.tsx
@@ -30,4 +30,30 @@ describe("box-shadow support", () => {
             "box-shadow: 5px 5px 50px rgba(0, 0, 0, 1)"
         )
     })
+
+    test("box-shadow should animate correctly, even when read from browser in weird format", async () => {
+        const promise = new Promise(resolve => {
+            const Component = () => (
+                <motion.div
+                    animate={{ boxShadow: "5px 5px 0px #fff" }}
+                    transition={{ duration: 0.05 }}
+                    onAnimationComplete={resolveContainer}
+                    style={{ boxShadow: "rgb(0, 0, 0) 5px 5px 50px 0px" }}
+                />
+            )
+
+            const resolveContainer = () => {
+                requestAnimationFrame(() => {
+                    resolve(container.firstChild as Element)
+                })
+            }
+
+            const { container, rerender } = render(<Component />)
+            rerender(<Component />)
+        })
+
+        return expect(promise).resolves.toHaveStyle(
+            "box-shadow: 5px 5px 0px rgba(255, 255, 255, 1)"
+        )
+    })
 })
