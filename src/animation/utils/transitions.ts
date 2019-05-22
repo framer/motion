0123e9e58f5c97e977bab7da2b215460a13cee28
InventingWithMonster <mattgperry@gmail.com>
2019-05-22 17:27:13 +0200

    fixing incorrect commit

--- src/animation/utils/transitions.ts
+++ src/animation/utils/transitions.ts
@@ -176,7 +176,6 @@ export function startAnimation(
     target: ResolvedValueTarget,
     { delay = 0, ...transition }: Transition
 ) {
-    delay = transition && transition.delay ? transition.delay : delay
     const [animationFactory, opts] = getAnimation(
         key,
         value,
