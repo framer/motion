824b7efac7d8baa01cbce0332c0bbe823f1d7de5
InventingWithMonster <mattgperry@gmail.com>
2019-07-29 12:06:31 +0200

    Adding explanatory comments

--- src/motion/utils/apply-exit-props.ts
+++ src/motion/utils/apply-exit-props.ts
@@ -11,7 +11,13 @@ export function applyExitProps<P>(
 
         return {
             ...props,
+            /**
+             * Overwrite user-defined `custom` with the one incoming from `AnimatePresence`.
+             * This will only be defined when a component is exiting and it allows a user
+             * to update `custom` even when a component has been removed from the tree.
+             */
             custom: custom !== undefined ? custom : props.custom,
+            // Animate to `exit` just by overwriting `animate`.
             animate: props.exit,
             onAnimationComplete: () => {
                 onExitComplete && onExitComplete()
