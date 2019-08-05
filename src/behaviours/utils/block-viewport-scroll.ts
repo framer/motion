5276072831f4678286241b9ed9888fd3e977abde
InventingWithMonster <mattgperry@gmail.com>
2019-08-05 15:19:42 +0200

    wrapping addEventListener in browser check

--- src/behaviours/utils/block-viewport-scroll.ts
+++ src/behaviours/utils/block-viewport-scroll.ts
@@ -1,14 +1,17 @@
 let isViewportScrollBlocked = false
+const isBrowser = typeof window !== "undefined"
 
-window.addEventListener(
-    "touchmove",
-    (event: TouchEvent) => {
-        if (isViewportScrollBlocked) {
-            event.preventDefault()
-        }
-    },
-    { passive: false }
-)
+if (isBrowser) {
+    window.addEventListener(
+        "touchmove",
+        (event: TouchEvent) => {
+            if (isViewportScrollBlocked) {
+                event.preventDefault()
+            }
+        },
+        { passive: false }
+    )
+}
 
 export const blockViewportScroll = () => (isViewportScrollBlocked = true)
 export const unblockViewportScroll = () => (isViewportScrollBlocked = false)
