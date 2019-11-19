623fdad10c5727679ef41f04ab67c68e8a2bdda6
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-11-19 10:25:06 +0100

    Fix mutating handler refs in render

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,4 +3,3 @@ import "jest-dom/extend-expect"
 // because @testing-library/react one switches out mouseEnter and mouseLeave
 import { fireEvent } from "@testing-library/dom"
 export const { mouseEnter, mouseLeave } = fireEvent
-import "@testing-library/react/cleanup-after-each"
