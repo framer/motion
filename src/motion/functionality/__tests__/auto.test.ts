3fad6f1d02672cd746169e4297df704692e0bf41
InventingWithMonster <mattgperry@gmail.com>
2020-02-20 16:36:09 +0100

    Adding more auto layout tests

--- src/motion/functionality/__tests__/auto.test.ts
+++ src/motion/functionality/__tests__/auto.test.ts
@@ -1,3 +1,5 @@
+import { clamp, progress } from "@popmotion/popcorn"
+
 interface Axis {
     min: number
     max: number
@@ -6,6 +8,7 @@ interface Axis {
 interface Delta {
     translate: number
     scale: number
+    origin: number
     originPoint: number
 }
 
@@ -19,25 +22,40 @@ const aAfter: Axis = {
     max: 200,
 }
 
-const bBefore: Axis = {
-    min: 200,
-    max: 300,
+// const bBefore: Axis = {
+//     min: 200,
+//     max: 300,
+// }
+
+// const bAfter: Axis = {
+//     min: 200,
+//     max: 300,
+// }
+
+const cBefore: Axis = {
+    min: 300,
+    max: 500,
 }
 
-const bAfter: Axis = {
-    min: 200,
-    max: 300,
+const cAfter: Axis = {
+    min: 350,
+    max: 450,
 }
+
+const clampProgress = clamp(0, 1)
 
 function calcOrigin(before: Axis, after: Axis): number {
     let origin = 0.5
-    const minDelta = before.min - after.min
-    const maxDelta = before.max - after.max
+    const beforeSize = before.max - before.min
+    const afterSize = after.max - after.min
 
-    // Hasn't changed size, just return center point
-    if (minDelta === maxDelta) return origin
+    if (beforeSize > afterSize) {
+        origin = progress(before.min, before.max - afterSize, after.min)
+    } else {
+        origin = progress(after.min, after.max - beforeSize, before.min)
+    }
 
-    return origin
+    return clampProgress(origin)
 }
 
 function calcDelta(before: Axis, after: Axis): Delta {
@@ -46,11 +64,13 @@ function calcDelta(before: Axis, after: Axis): Delta {
 
     const scale = beforeSize / afterSize
     const origin = calcOrigin(before, after)
+    const originPoint = after.min + origin * afterSize
 
     return {
         scale,
         translate: -1,
-        originPoint: 100,
+        origin,
+        originPoint,
     }
 }
 
@@ -60,37 +80,54 @@ function scaledPoint({ scale, originPoint }: Delta, point: number) {
     return originPoint + scaled
 }
 
-function applyDelta(delta: Delta, axis: Axis) {
-    let min = axis.min
-    let max = axis.max
+// function applyDelta(delta: Delta, axis: Axis) {
+//     let min = axis.min
+//     let max = axis.max
 
-    min = scaledPoint(delta, axis.min)
-    max = scaledPoint(delta, axis.max)
+//     min = scaledPoint(delta, axis.min)
+//     max = scaledPoint(delta, axis.max)
 
-    return { min, max }
-}
+//     return { min, max }
+// }
 
 test("scaledPoint", () => {
     expect(
-        scaledPoint({ scale: 2, originPoint: 0, translate: 0 }, 100)
+        scaledPoint({ scale: 2, originPoint: 0, origin: 0, translate: 0 }, 100)
     ).toEqual(200)
     expect(
-        scaledPoint({ scale: 0.5, originPoint: 0, translate: 0 }, 100)
+        scaledPoint(
+            { scale: 0.5, originPoint: 0, origin: 0, translate: 0 },
+            100
+        )
     ).toEqual(50)
     expect(
-        scaledPoint({ scale: 3, originPoint: 500, translate: 0 }, 100)
+        scaledPoint(
+            { scale: 3, originPoint: 500, origin: 0, translate: 0 },
+            100
+        )
     ).toEqual(-700)
 })
 
-test("transforms", () => {
-    const aDelta = calcDelta(aBefore, aAfter)
+test("calcOrigin", () => {
+    expect(calcOrigin({ min: 0, max: 100 }, { min: 0, max: 100 })).toBe(1)
+    expect(calcOrigin({ min: -100, max: 100 }, { min: -50, max: 50 })).toBe(0.5)
+    expect(calcOrigin({ min: -50, max: 50 }, { min: -100, max: 100 })).toBe(0.5)
+    expect(calcOrigin({ min: 200, max: 200 }, { min: 0, max: 100 })).toBe(1)
+    expect(calcOrigin({ min: 200, max: 200 }, { min: 300, max: 500 })).toBe(0)
+})
 
-    expect(aDelta).toEqual({
+test("calcDelta", () => {
+    expect(calcDelta(aBefore, aAfter)).toEqual({
         translate: -1,
-        origin: 0.5,
-        originPoint: 100,
+        origin: 0,
+        originPoint: 0,
         scale: 1.5,
     })
 
-    expect(applyDelta(aDelta, aAfter)).toEqual(aBefore)
+    expect(calcDelta(cBefore, cAfter)).toEqual({
+        translate: -1,
+        origin: 0.5,
+        originPoint: 400,
+        scale: 2,
+    })
 })
