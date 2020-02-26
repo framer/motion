d45b6127bb941929f52c357475a9443865c93b74
InventingWithMonster <mattgperry@gmail.com>
2020-02-26 16:29:29 +0100

    Fixing tests

--- src/utils/__tests__/is-numerical-string.test.ts
+++ src/utils/__tests__/is-numerical-string.test.ts
@@ -4,9 +4,10 @@ describe("isNumericalString", () => {
     test("should correctly identify numerical strings", () => {
         expect(isNumericalString("10")).toBe(true)
         expect(isNumericalString("10.1")).toBe(true)
-        expect(isNumericalString("10px")).toBe(true)
-        expect(isNumericalString("10%")).toBe(true)
-        expect(isNumericalString("10.1%")).toBe(true)
+        expect(isNumericalString("-10.1")).toBe(true)
+        expect(isNumericalString("10px")).toBe(false)
+        expect(isNumericalString("10%")).toBe(false)
+        expect(isNumericalString("10.1%")).toBe(false)
         expect(isNumericalString("rgb(0,0,0)")).toBe(false)
     })
 })
