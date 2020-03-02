cf85472d9ebdca268c9710b09095b9223ded6fd1
InventingWithMonster <mattgperry@gmail.com>
2020-03-02 14:09:48 +0100

    Adding example

--- src/components/AnimatePresence/use-presence.ts
+++ src/components/AnimatePresence/use-presence.ts
@@ -5,6 +5,24 @@ type Present = [true]
 
 type NotPresent = [false, () => void]
 
+/**
+ * When a component is the child of an `AnimatePresence` component, it has access to
+ * information about whether it's still present the React tree. `usePresence` can be
+ * used to access that data and perform operations before the component can be considered
+ * safe to remove.
+ *
+ * When `isPresent` is `true`, the `safeToRemove` callback is `undefined`.
+ *
+ * ```jsx
+ * const [isPresent, safeToRemove] = usePresence()
+ *
+ * useEffect(() => {
+ *   !isPresent setTimeout(safeToRemove, 1000)
+ * }, [isPresent])
+ * ```
+ *
+ * @public
+ */
 export function usePresence(): Present | NotPresent {
     const { exitProps } = useContext(MotionContext)
 
