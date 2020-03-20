b25482aab13d7536c9df6747b99b2125ce0ecf47
InventingWithMonster <mattgperry@gmail.com>
2020-03-20 15:30:39 +0100

    Fixing FLIP animations

--- src/motion/functionality/exit.ts
+++ src/motion/functionality/exit.ts
@@ -8,10 +8,7 @@ import { PresenceContext } from "../../components/AnimatePresence/PresenceContex
 
 export const Exit: FunctionalComponentDefinition = {
     key: "exit",
-    shouldRender: props => {
-        const isMagic = !!props.magic || props.magicId !== undefined
-        return isMagic || (!!props.exit && !checkShouldInheritVariant(props))
-    },
+    shouldRender: props => !!props.exit && !checkShouldInheritVariant(props),
     Component: makeRenderlessComponent((props: FunctionalProps) => {
         const { animate, controls, exit } = props
         const [isPresent, onExitComplete] = usePresence()
