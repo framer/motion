b1548583766a7a200b0f81e586c332b647460ebf
InventingWithMonster <mattgperry@gmail.com>
2020-05-14 14:05:24 +0200

    Fixing layoutOrder and crossfade in Framer

--- src/components/AnimateSharedLayout/index.tsx
+++ src/components/AnimateSharedLayout/index.tsx
@@ -186,7 +186,7 @@ export class AnimateSharedLayout extends React.Component<
         if (layoutId === undefined) return
 
         const stack = this.getStack(layoutId)
-        stack.add(child, this.hasMounted)
+        stack.add(child)
     }
 
     removeChild(child: Auto) {
