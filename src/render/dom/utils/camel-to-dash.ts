073266b7163ec215ca463399129b963f6d79215e
InventingWithMonster <mattgperry@gmail.com>
2020-06-10 15:45:32 +0200

    Latest

---
+++ src/render/dom/utils/camel-to-dash.ts
@@ -0,0 +1,5 @@
+const CAMEL_CASE_PATTERN = /([a-z])([A-Z])/g
+const REPLACE_TEMPLATE = "$1-$2"
+
+export const camelToDash = (str: string) =>
+    str.replace(CAMEL_CASE_PATTERN, REPLACE_TEMPLATE).toLowerCase()
