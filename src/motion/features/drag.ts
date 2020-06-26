abf6c414b624c7612febf752179727ba4415d6a9
InventingWithMonster <mattgperry@gmail.com>
2020-06-26 08:34:32 +0200

    Rendering drag component if only dragConstraints is defined

--- src/motion/features/drag.ts
+++ src/motion/features/drag.ts
@@ -5,7 +5,8 @@ import { FeatureProps, MotionFeature } from "./types"
 
 export const Drag: MotionFeature = {
     key: "drag",
-    shouldRender: (props: MotionProps) => !!props.drag,
+    shouldRender: (props: MotionProps) =>
+        !!props.drag || !!props.dragConstraints,
     Component: makeRenderlessComponent(
         ({ visualElement, controls, ...props }: FeatureProps) => {
             return useDrag(props, visualElement, controls)
