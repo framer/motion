c7e9fe06ab08a1c72e6de124127540f7c598eaaa
InventingWithMonster <mattgperry@gmail.com>
2020-07-14 11:29:55 +0200

    Updating examples template

--- dev/styled.ts
+++ dev/styled.ts
@@ -3,8 +3,7 @@ import styled from "styled-components"
 export const Container = styled.div`
     min-width: 100vw;
     min-height: 100vh;
-    background: linear-gradient(0deg, rgb(119, 0, 255), rgba(119, 0, 255, 0)),
-        linear-gradient(90deg, rgb(255, 0, 136), rgb(0, 153, 255));
+    background: linear-gradient(250deg, #7b2ff7, #f107a3);
     display: flex;
     align-items: center;
     justify-content: center;
