53f4940dc5e98b43235082e1ba53e89b7e109423
InventingWithMonster <mattgperry@gmail.com>
2020-07-23 12:18:48 +0200

    Upgrading Style Value Types with hsla fixes

--- dev/examples/Events-whileHover.tsx
+++ dev/examples/Events-whileHover.tsx
@@ -8,13 +8,16 @@ import { motion } from "@framer"
 const style = {
     width: 100,
     height: 100,
-    background: "white",
 }
 
 export const App = () => {
     return (
         <motion.div
-            whileHover={{ scale: 1.5 }}
+            initial={{ backgroundColor: "hsla(0  25.5%  50% / 1)" }}
+            whileHover={{
+                scale: 1.5,
+                backgroundColor: "hsla(177 37.4978% 76.66804% / 0.5)",
+            }}
             whileTap={{ scale: 0.5 }}
             style={style}
         />
