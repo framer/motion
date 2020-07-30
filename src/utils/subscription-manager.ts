936b3f1123c15c987aabd2b05a3f36422d135ed0
InventingWithMonster <mattgperry@gmail.com>
2020-07-30 13:32:43 +0200

    Updating build process

--- src/utils/subscription-manager.ts
+++ src/utils/subscription-manager.ts
@@ -1,27 +1,58 @@
+/**
+ *
+ */
+// export class SubscriptionManager<Handler> {
+//     private subscriptions: Handler[] = []
+//     private numSubscriptions: number = 0
+
+//     private updateSubscriptionCount() {
+//         this.numSubscriptions = this.subscriptions.length
+//     }
+
+//     private findHandlerIndex(handler: Handler) {
+//         return this.subscriptions.findIndex(h => h === handler)
+//     }
+
+//     subscribe(handler: Handler) {
+//         let i = this.findHandlerIndex(handler)
+//         i === -1 && this.subscriptions.push(handler)
+//         this.updateSubscriptionCount()
+
+//         return () => {
+//             i = this.findHandlerIndex(handler)
+//             i !== -1 && this.subscriptions.splice(i, 1)
+//             this.updateSubscriptionCount()
+//         }
+//     }
+
+//     notify(a?: any, b?: any, c?: any) {
+//         if (!this.numSubscriptions) return
+
+//         for (let i = 0; i < this.numSubscriptions; i++) {
+//             ;(this.subscriptions[i] as any)(a, b, c)
+//         }
+//     }
+
+//     clear() {
+//         this.subscriptions.length = 0
+//         this.updateSubscriptionCount()
+//     }
+// }
+
 export class SubscriptionManager<Handler> {
-    private subscriptions: Handler[] = []
-    private numSubscriptions: number = 0
+    private subscriptions = new Set<Handler>()
+    private numSubscriptions = 0
 
     private updateSubscriptionCount() {
-        this.numSubscriptions = this.subscriptions.length
-    }
-
-    private findHandlerIndex(handler: Handler) {
-        return this.subscriptions.findIndex(h => h === handler)
-    }
-
-    private containsHandler(handler: Handler) {
-        const i = this.findHandlerIndex(handler)
-        return i !== -1
+        this.numSubscriptions = this.subscriptions.size
     }
 
     subscribe(handler: Handler) {
-        !this.containsHandler(handler) && this.subscriptions.push(handler)
+        this.subscriptions.add(handler)
         this.updateSubscriptionCount()
 
         return () => {
-            const i = this.findHandlerIndex(handler)
-            i !== -1 && this.subscriptions.splice(i, 1)
+            this.subscriptions.delete(handler)
             this.updateSubscriptionCount()
         }
     }
@@ -29,13 +60,13 @@ export class SubscriptionManager<Handler> {
     notify(a?: any, b?: any, c?: any) {
         if (!this.numSubscriptions) return
 
-        for (let i = 0; i < this.numSubscriptions; i++) {
-            ;(this.subscriptions[i] as any)(a, b, c)
+        for (const handler of this.subscriptions) {
+            ;(handler as any)(a, b, c)
         }
     }
 
     clear() {
-        this.subscriptions = []
+        this.subscriptions.clear()
         this.updateSubscriptionCount()
     }
 }
