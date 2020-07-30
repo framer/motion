d2c172fe0119c0b11f6a4d5926ac8a16a82ddf11
InventingWithMonster <mattgperry@gmail.com>
2020-07-30 15:24:45 +0200

    Updating tests

--- src/value/__tests__/use-spring.test.tsx
+++ src/value/__tests__/use-spring.test.tsx
@@ -69,6 +69,6 @@ describe("useSpring", () => {
         rerender(<Component target={a} />)
         rerender(<Component target={a} />)
 
-        expect((a!.updateSubscribers! as any).numSubscriptions).toBe(1)
+        expect((a!.updateSubscribers! as any).subscriptions.size).toBe(1)
     })
 })
