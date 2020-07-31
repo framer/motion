15be3167149b80f53fa79b067ff09e75c98d8e69
InventingWithMonster <mattgperry@gmail.com>
2020-07-31 10:40:41 +0200

    Fixing function call

--- src/utils/subscription-manager.ts
+++ src/utils/subscription-manager.ts
@@ -16,13 +16,13 @@ export class SubscriptionManager<Handler extends GenericHandler> {
          * Using ...args would be preferable but it's array creation and this
          * might be fired every frame.
          */
-        _a?: Parameters<Handler>[0],
-        _b?: Parameters<Handler>[1],
-        _c?: Parameters<Handler>[2]
+        a?: Parameters<Handler>[0],
+        b?: Parameters<Handler>[1],
+        c?: Parameters<Handler>[2]
     ) {
         if (!this.subscriptions.size) return
         for (const handler of this.subscriptions) {
-            handler.call(arguments)
+            handler(a, b, c)
         }
     }
 
