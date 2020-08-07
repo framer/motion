1abd09fd2f5f19901db96dd4e82c64ccb9c15633
Adam Seckel <adam.seckel@gmail.com>
2020-08-07 15:22:32 +0100

    remove commented code

--- src/components/SharedLayoutTree.tsx
+++ src/components/SharedLayoutTree.tsx
@@ -16,46 +16,6 @@ export const SharedLayoutTreeContext = React.createContext<TreeConfig>(
     undefined
 )
 
-// export const SharedLayoutTree = ({
-//     enabled,
-//     order,
-//     lead,
-//     present,
-//     ...rest
-// }: React.PropsWithChildren<{
-//     order?: number
-//     enabled?: boolean
-//     lead?: boolean
-//     present?: false | undefined
-// }>) => {
-//     const config = React.useRef<SharedLayoutTreeConfig>({
-//         enabled,
-//         layoutOrder: undefined,
-//         lead: undefined,
-//     })
-
-//     const [forcedUpdate, forceTreeUpdate] = React.useState(0)
-
-//     React.useEffect(() => {
-//         const wasLead = config.current.lead
-
-//         config.current.lead = lead
-//         config.current.layoutOrder = order
-//         config.current.enabled = enabled
-
-//         // Right now this is consumed by Measure components to trigger updates.
-//         // Perhaps it can be done in a better way.
-//         if (!wasLead && lead) forceTreeUpdate(value => value + 1)
-//     }, [enabled, order, lead])
-
-//     return (
-//         <SharedLayoutTreeContext.Provider
-//             value={{ forcedUpdate, treeConfig: config }}
-//             {...rest}
-//         />
-//     )
-// }
-
 type Props = React.PropsWithChildren<{
     order?: number
     enabled?: boolean
