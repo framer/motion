87333ad1b11c48892ddea1edc5b5328603063bef
Adam Seckel <adam.seckel@gmail.com>
2020-08-13 21:09:52 +0100

    Some updates

--- src/motion/features/layout/Animate.tsx
+++ src/motion/features/layout/Animate.tsx
@@ -96,9 +96,10 @@ class Animate extends React.Component<AnimateProps> {
                 visibilityAction === VisibilityAction.Hide
                     ? visualElement.hide()
                     : visualElement.show()
-            } else if (boxHasMoved) {
+            } else if (boxHasMoved || config.crossfadeOpacity) {
                 // If the box has moved, animate between it's current visual state and its
                 // final state
+                console.log("running animate axis", visualElement.layoutId)
                 return this.animateAxis(
                     axis,
                     target[axis],
