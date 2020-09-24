b4319c78fb4bde28ce0d2a8008df48d7e3fd1c8b
InventingWithMonster <mattgperry@gmail.com>
2020-09-24 17:35:56 +0200

    Updating approach

--- src/render/dom/index.ts
+++ src/render/dom/index.ts
@@ -23,6 +23,23 @@ export type CustomDomComponent<Props> = React.ForwardRefExoticComponent<
         React.RefAttributes<SVGElement | HTMLElement>
 >
 
+const allMotionFeatures = [
+    MeasureLayout,
+    Animation,
+    Drag,
+    Gestures,
+    Exit,
+    AnimateLayout,
+]
+
+const domBaseConfig = {
+    useVisualElement: useDomVisualElement as any,
+    render: render as any,
+    animationControlsConfig: {
+        makeTargetAnimatable: parseDomVariant,
+    },
+}
+
 /**
  * Convert any React component into a `motion` component. The provided component
  * **must** use `React.forwardRef` to the underlying DOM component you want to animate.
@@ -37,7 +54,6 @@ export type CustomDomComponent<Props> = React.ForwardRefExoticComponent<
  *
  * @public
  */
-
 export function createMotionProxy(defaultFeatures: MotionFeature[]) {
     type CustomMotionComponent = { custom: typeof custom }
     type Motion = HTMLMotionComponents &
@@ -45,12 +61,8 @@ export function createMotionProxy(defaultFeatures: MotionFeature[]) {
         CustomMotionComponent
 
     const config: MotionComponentConfig<HTMLElement | SVGElement> = {
+        ...domBaseConfig,
         defaultFeatures,
-        useVisualElement: useDomVisualElement as any,
-        render: render as any,
-        animationControlsConfig: {
-            makeTargetAnimatable: parseDomVariant,
-        },
     }
 
     function custom<Props>(
@@ -73,15 +85,6 @@ export function createMotionProxy(defaultFeatures: MotionFeature[]) {
     return new Proxy({ custom }, { get }) as Motion
 }
 
-export const allMotionFeatures = [
-    MeasureLayout,
-    Animation,
-    Drag,
-    Gestures,
-    Exit,
-    AnimateLayout,
-]
-
 /**
  * HTML & SVG components, optimised for use with gestures and animation. These can be used as
  * drop-in replacements for any HTML & SVG component, all CSS & SVG properties are supported.
@@ -89,3 +92,25 @@ export const allMotionFeatures = [
  * @public
  */
 export const motion = /*@__PURE__*/ createMotionProxy(allMotionFeatures)
+
+/**
+ * Create a DOM `motion` component with the provided string. This is primarily intended
+ * as a full alternative to `motion` for consumers who have to support environments that don't
+ * support `Proxy`.
+ *
+ * ```javascript
+ * import { createDomMotionComponent } from "framer-motion"
+ *
+ * const motion = {
+ *   div: createDomMotionComponent('div')
+ * }
+ * ```
+ *
+ * @public
+ */
+export function createDomMotionComponent(key: string) {
+    return createMotionComponent(key, {
+        ...domBaseConfig,
+        defaultFeatures: allMotionFeatures,
+    })
+}
