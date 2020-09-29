1adc3d2c2b4aca4b0f9c7e3ae497fd184916e0a1
InventingWithMonster <mattgperry@gmail.com>
2020-09-29 10:20:03 +0200

    Create a new MotionValue from style  when an old one is removed from style

--- src/motion/__tests__/style-prop.test.tsx
+++ src/motion/__tests__/style-prop.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "../../../jest.setup"
-import { motion } from "../.."
+import { motion, useMotionValue } from "../.."
 import * as React from "react"
 
 describe("style prop", () => {
@@ -16,6 +16,60 @@ describe("style prop", () => {
 
         expect(container.firstChild as Element).not.toHaveStyle(
             "position: absolute"
+        )
+    })
+
+    test("should updated transforms when passed a new value", () => {
+        const Component = ({ x = 0 }) => {
+            return <motion.div style={{ x }} />
+        }
+
+        const { container, rerender } = render(<Component />)
+
+        expect(container.firstChild as Element).toHaveStyle("transform: none")
+
+        rerender(<Component x={1} />)
+
+        expect(container.firstChild as Element).toHaveStyle(
+            "transform: translateX(1px) translateZ(0)"
+        )
+
+        rerender(<Component x={0} />)
+
+        expect(container.firstChild as Element).toHaveStyle("transform: none")
+    })
+
+    test("should update when passed new MotionValue", () => {
+        const Component = ({ useX = false }) => {
+            const x = useMotionValue(1)
+            const y = useMotionValue(2)
+
+            return (
+                <motion.div
+                    style={{
+                        x: useX ? x : 0,
+                        y: !useX ? y : 0,
+                    }}
+                />
+            )
+        }
+
+        const { container, rerender } = render(<Component />)
+
+        expect(container.firstChild as Element).toHaveStyle(
+            "transform: translateX(0px) translateY(2px) translateZ(0)"
+        )
+
+        rerender(<Component useX />)
+
+        expect(container.firstChild as Element).toHaveStyle(
+            "transform: translateX(1px) translateY(0px) translateZ(0)"
+        )
+
+        rerender(<Component />)
+
+        expect(container.firstChild as Element).toHaveStyle(
+            "transform: translateX(0px) translateY(2px) translateZ(0)"
         )
     })
 })
