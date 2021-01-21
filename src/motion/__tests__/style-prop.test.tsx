5d7f687726bddb824329b30e7aa050a24d53a3f2
InventingWithMonster <mattgperry@gmail.com>
2021-01-21 11:02:25 +0100

    Latest

--- src/motion/__tests__/style-prop.test.tsx
+++ src/motion/__tests__/style-prop.test.tsx
@@ -1,27 +1,27 @@
 import { render } from "../../../jest.setup"
-import { motion } from "../.."
+import { motion, MotionConfig, useMotionValue } from "../.."
 import * as React from "react"
 
 describe("style prop", () => {
-    // test("should remove non-set styles", () => {
-    //     function Component({ style }: any) {
-    //         return (
-    //             <MotionConfig isStatic>
-    //                 <motion.div data-testid="child" style={style} />
-    //             </MotionConfig>
-    //         )
-    //     }
+    test("should remove non-set styles", () => {
+        function Component({ style }: any) {
+            return (
+                <MotionConfig isStatic>
+                    <motion.div data-testid="child" style={style} />
+                </MotionConfig>
+            )
+        }
 
-    //     const { getByTestId, rerender } = render(
-    //         <Component style={{ position: "absolute" }} />
-    //     )
+        const { getByTestId, rerender } = render(
+            <Component style={{ position: "absolute" }} />
+        )
 
-    //     expect(getByTestId("child")).toHaveStyle("position: absolute")
+        expect(getByTestId("child")).toHaveStyle("position: absolute")
 
-    //     rerender(<Component style={{}} />)
+        rerender(<Component style={{}} />)
 
-    //     expect(getByTestId("child")).not.toHaveStyle("position: absolute")
-    // })
+        expect(getByTestId("child")).not.toHaveStyle("position: absolute")
+    })
 
     test("should update transforms when passed a new value", () => {
         const Component = ({ x = 0 }) => {
@@ -34,8 +34,6 @@ describe("style prop", () => {
 
         rerender(<Component x={1} />)
 
-        console.log("should be x: 1")
-
         expect(container.firstChild as Element).toHaveStyle(
             "transform: translateX(1px) translateZ(0)"
         )
@@ -45,39 +43,39 @@ describe("style prop", () => {
         expect(container.firstChild as Element).toHaveStyle("transform: none")
     })
 
-    // test("should update when passed new MotionValue", async () => {
-    //     const Component = ({ useX = false }) => {
-    //         const x = useMotionValue(1)
-    //         const y = useMotionValue(2)
-    //         const z = useMotionValue(3)
+    test("should update when passed new MotionValue", async () => {
+        const Component = ({ useX = false }) => {
+            const x = useMotionValue(1)
+            const y = useMotionValue(2)
+            const z = useMotionValue(3)
 
-    //         return (
-    //             <motion.div
-    //                 style={{
-    //                     x: useX ? x : 0,
-    //                     y: !useX ? y : 0,
-    //                     z: !useX ? z : 0,
-    //                 }}
-    //             />
-    //         )
-    //     }
+            return (
+                <motion.div
+                    style={{
+                        x: useX ? x : 0,
+                        y: !useX ? y : 0,
+                        z: !useX ? z : 0,
+                    }}
+                />
+            )
+        }
 
-    //     const { container, rerender } = render(<Component />)
+        const { container, rerender } = render(<Component />)
 
-    //     expect(container.firstChild as Element).toHaveStyle(
-    //         "transform: translateX(0px) translateY(2px) translateZ(3px)"
-    //     )
+        expect(container.firstChild as Element).toHaveStyle(
+            "transform: translateX(0px) translateY(2px) translateZ(3px)"
+        )
 
-    //     rerender(<Component useX />)
+        rerender(<Component useX />)
 
-    //     expect(container.firstChild as Element).toHaveStyle(
-    //         "transform: translateX(1px) translateY(0px) translateZ(0px)"
-    //     )
+        expect(container.firstChild as Element).toHaveStyle(
+            "transform: translateX(1px) translateY(0px) translateZ(0px)"
+        )
 
-    //     rerender(<Component />)
+        rerender(<Component />)
 
-    //     expect(container.firstChild as Element).toHaveStyle(
-    //         "transform: translateX(0px) translateY(2px) translateZ(3px)"
-    //     )
-    // })
+        expect(container.firstChild as Element).toHaveStyle(
+            "transform: translateX(0px) translateY(2px) translateZ(3px)"
+        )
+    })
 })
