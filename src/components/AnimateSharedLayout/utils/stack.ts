74c99c13d00972938a12cb82489337e367ba0d8d
InventingWithMonster <mattgperry@gmail.com>
2021-02-01 19:08:19 +0100

    allowing preservation of opacity

--- src/components/AnimateSharedLayout/utils/stack.ts
+++ src/components/AnimateSharedLayout/utils/stack.ts
@@ -20,90 +20,6 @@ export type LeadAndFollow = [
     VisualElement | undefined
 ]
 
-/**
- * For each layout animation, we want to identify two components
- * within a stack that will serve as the "lead" and "follow" components.
- *
- * In the switch animation, the lead component performs the entire animation.
- * It uses the follow bounding box to animate out from and back to. The follow
- * component is hidden.
- *
- * In the crossfade animation, both the lead and follow components perform
- * the entire animation, animating from the follow origin bounding box to the lead
- * target bounding box.
- *
- * Generalising a stack as First In Last Out, *searching from the end* we can
- * generally consider the lead component to be:
- *  - If the last child is present, the last child
- *  - If the last child is exiting, the last *encountered* exiting component
- */
-// export function findLeadAndFollow(
-//     stack: VisualElement[],
-//     [prevLead, prevFollow]: LeadAndFollow
-// ): LeadAndFollow {
-//     let lead: VisualElement | undefined = undefined
-//     let leadIndex = 0
-//     let follow: VisualElement | undefined = undefined
-
-//     // Find the lead child first
-//     const numInStack = stack.length
-//     let lastIsPresent: boolean | undefined = false
-
-//     for (let i = numInStack - 1; i >= 0; i--) {
-//         const child = stack[i]
-
-//         const isLastInStack = i === numInStack - 1
-//         if (isLastInStack) lastIsPresent = child.isPresent
-
-//         if (lastIsPresent) {
-//             lead = child
-//         } else {
-//             // If the child before this will be present, make this the
-//             // lead.
-//             const prev = stack[i - 1]
-//             if (prev && prev.isPresent) lead = child
-//         }
-
-//         if (lead) {
-//             leadIndex = i
-//             break
-//         }
-//     }
-
-//     if (!lead) lead = stack[0]
-
-//     // Find the follow child
-//     follow = stack[leadIndex - 1]
-
-//     // If the lead component is exiting, find the closest follow
-//     // present component
-//     if (lead) {
-//         for (let i = leadIndex - 1; i >= 0; i--) {
-//             const child = stack[i]
-//             if (child.isPresent) {
-//                 follow = child
-//                 break
-//             }
-//         }
-//     }
-
-//     // If the lead has changed and the previous lead still exists in the
-//     // stack, set it to the previous lead. This allows us to differentiate between
-//     // a, b, c(exit) -> a, b(exit), c(exit)
-//     // and
-//     // a, b(exit), c -> a, b(exit), c(exit)
-//     if (
-//         lead !== prevLead &&
-//         !lastIsPresent &&
-//         follow === prevFollow &&
-//         stack.find((stackChild) => stackChild === prevLead)
-//     ) {
-//         lead = prevLead
-//     }
-
-//     return [lead, follow]
-// }
-
 export interface LayoutStack {
     add(element: VisualElement): void
     remove(element: VisualElement): void
@@ -118,7 +34,10 @@ export interface CrossfadeState {
     isCrossfading(): boolean
     getValues(element: VisualElement): VisualState
     crossfadeFromLead(transition?: Transition): void
-    crossfadeToLead(transition?: Transition): void
+    crossfadeToLead(
+        transition?: Transition,
+        maintainFollowOpacity: boolean
+    ): void
     reset(): void
     stopAnimation(): void
 }
@@ -132,6 +51,7 @@ export function createCrossfadeState(
     let leadValues = createVisualState({})
     const crossfadeProgress = motionValue(1)
     let lastUpdate = 0
+    let maintainFollowOpacity = false
 
     function animateProgress(to: number, transition?: Transition) {
         const update = () => {
@@ -161,11 +81,9 @@ export function createCrossfadeState(
             const leadTargetOpacity = lead.getStaticValue("opacity") ?? 1
 
             // TODO Make this configurable by crossfade
-            followValues.values.opacity = mix(
-                followTargetOpacity as number,
-                0,
-                easeCrossfadeOut(p)
-            )
+            followValues.values.opacity = maintainFollowOpacity
+                ? followTargetOpacity
+                : mix(followTargetOpacity as number, 0, easeCrossfadeOut(p))
             leadValues.values.opacity = mix(
                 0,
                 leadTargetOpacity as number,
@@ -184,7 +102,8 @@ export function createCrossfadeState(
             followValues = createVisualState({})
             leadValues = createVisualState({})
         },
-        crossfadeToLead(transition) {
+        crossfadeToLead(transition, newMaintainFollowOpacity = false) {
+            maintainFollowOpacity = newMaintainFollowOpacity
             crossfadeProgress.set(1 - crossfadeProgress.get())
             animateProgress(1, transition)
             followValues = createVisualState({})
@@ -362,60 +281,3 @@ function mixBorderRadius(
         )
     }
 }
-
-// export class LayoutStack {
-//     order: VisualElement[] = []
-
-//     lead?: VisualElement | undefined
-//     follow?: VisualElement | undefined
-
-//     prevLead?: VisualElement | undefined
-//     prevFollow?: VisualElement | undefined
-
-//     snapshot?: Snapshot
-
-//     getLead() {
-//         return this.lead
-//     }
-
-//     updateLeadAndFollow() {
-//         this.prevLead = this.lead
-//         this.prevFollow = this.follow
-
-//         const [lead, follow] = findLeadAndFollow(this.order, [
-//             this.lead,
-//             this.follow,
-//         ])
-
-//         this.lead = lead
-//         this.follow = follow
-//     }
-
-//     // updateSnapshot() {
-//     //     if (!this.lead) return
-
-//     //     const snapshot: Snapshot = {
-//     //         boundingBox: this.lead.getProjection().target,
-//     //         latestMotionValues: {},
-//     //     }
-
-//     //     this.lead.forEachValue((value, key) => {
-//     //         const latest = value.get()
-//     //         if (!isTransformProp(latest)) {
-//     //             snapshot.latestMotionValues[key] = latest
-//     //         }
-//     //     })
-
-//     //     const dragControls = elementDragControls.get(this.lead)
-//     //     if (dragControls && dragControls.isDragging) {
-//     //         snapshot.isDragging = true
-//     //         snapshot.cursorProgress = dragControls.cursorProgress
-//     //     }
-
-//     //     this.snapshot = snapshot
-//     // }
-
-//     isLeadPresent() {
-//         return this.lead && this.lead?.presence !== Presence.Exiting
-//     }
-// }
