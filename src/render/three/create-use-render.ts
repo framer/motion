b82c684492ac56aafe874d7646ce018c3293dda1
InventingWithMonster <mattgperry@gmail.com>
2021-02-25 14:10:52 +0100

    Passing through props

--- src/render/three/create-use-render.ts
+++ src/render/three/create-use-render.ts
@@ -6,8 +6,8 @@ export function createThreeUseRender<Props>(
     Component: string | React.ComponentType<Props>,
     _forwardMotionProps = false
 ) {
-    const useRender = (_props: MotionProps, _visualElement: VisualElement) => {
-        return createElement(Component)
+    const useRender = (props: MotionProps, _visualElement: VisualElement) => {
+        return createElement(Component, props)
     }
     return useRender
 }
