1457e060a39b940d5cf7e4c50112618727959d3f
InventingWithMonster <mattgperry@gmail.com>
2021-02-25 16:46:11 +0100

    Changing background

--- dev/examples/ThreeBox.tsx
+++ dev/examples/ThreeBox.tsx
@@ -26,32 +26,41 @@ export const App = () => {
     const [show, setShow] = useState(true)
 
     return (
-        <Canvas colorManagement style={{ width: "100vw", height: "100vh" }}>
-            <mesh position={[2, 0, 0]} onClick={() => setShow(!show)}>
-                <meshBasicMaterial attach="material" />
-                <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-            </mesh>
+        <div style={{ position: "fixed", inset: 0, background: "white" }}>
+            <Canvas colorManagement style={{ width: "100vw", height: "100vh" }}>
+                <mesh position={[2, 0, 0]} onClick={() => setShow(!show)}>
+                    <meshBasicMaterial attach="material" />
+                    <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+                </mesh>
 
-            <AnimatePresence>
-                {show && (
-                    <motion.group>
-                        <motion.mesh
-                            variants={variants}
-                            initial={"gone"}
-                            exit={"gone"}
-                            animate={"small"}
-                            whileHover={"big"}
-                            transition={{ duration: 3 }}
-                        >
-                            <meshBasicMaterial attach="material" transparent />
-                            <boxBufferGeometry
-                                attach="geometry"
-                                args={[1, 1, 1]}
-                            />
-                        </motion.mesh>
-                    </motion.group>
-                )}
-            </AnimatePresence>
-        </Canvas>
+                <AnimatePresence>
+                    {show && (
+                        <motion.group>
+                            <motion.mesh
+                                variants={variants}
+                                initial={"gone"}
+                                exit={"gone"}
+                                animate={"small"}
+                                whileHover={"big"}
+                                transition={{
+                                    duration: 0.5,
+                                    type: "spring",
+                                    bounce: 0.3,
+                                }}
+                            >
+                                <meshBasicMaterial
+                                    attach="material"
+                                    transparent
+                                />
+                                <boxBufferGeometry
+                                    attach="geometry"
+                                    args={[1, 1, 1]}
+                                />
+                            </motion.mesh>
+                        </motion.group>
+                    )}
+                </AnimatePresence>
+            </Canvas>
+        </div>
     )
 }
