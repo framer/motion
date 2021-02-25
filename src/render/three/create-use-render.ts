397883c992dcb9d5757591b77b8ca6a9d136beb6
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-02-25 17:22:04 +0100

    Filter Motion props

--- src/render/three/create-use-render.ts
+++ src/render/three/create-use-render.ts
@@ -4,16 +4,24 @@ import * as ReactThreeFiber from "react-three-fiber"
 import { MotionProps } from "../../motion/types"
 import { VisualElement } from "../types"
 import { AnimationType } from "../utils/animation-state"
+import { isValidMotionProp } from "../../motion/utils/valid-prop"
+
+type Object3DProps = ReactThreeFiber.Object3DNode<
+    Three.Object3D,
+    typeof Three.Object3D
+>
+
+type Object3DMotionProps = Object3DProps & MotionProps
 
 export function createThreeUseRender<Props>(
     Component: string | React.ComponentType<Props>,
-    _forwardMotionProps = false
+    forwardMotionProps = false
 ) {
     const useRender = (props: MotionProps, visualElement: VisualElement) => {
         const hoverProps = useHoverGesture(props, visualElement)
 
         return createElement(Component, {
-            ...props,
+            ...filterProps(props, forwardMotionProps),
             ...hoverProps,
             ref: visualElement.ref,
         } as any)
@@ -21,12 +29,29 @@ export function createThreeUseRender<Props>(
     return useRender
 }
 
+function shouldForward(key: string) {
+    return !isValidMotionProp(key)
+}
+
+function filterProps(props: MotionProps, forwardMotionProps: boolean) {
+    const filteredProps = {}
+
+    for (const key in props) {
+        if (
+            shouldForward(key) ||
+            (forwardMotionProps === true && isValidMotionProp(key))
+        ) {
+            filteredProps[key] = props[key]
+        }
+    }
+
+    return filteredProps
+}
+
 function useHoverGesture(
     { whileHover }: MotionProps,
     visualElement: VisualElement
-): Partial<
-    ReactThreeFiber.Object3DNode<Three.Object3D, typeof Three.Object3D>
-> {
+): Object3DMotionProps {
     const onPointerOver = useCallback(() => {
         visualElement.animationState?.setActive(AnimationType.Hover, true)
     }, [visualElement])
