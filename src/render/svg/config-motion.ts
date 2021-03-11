ab68333005a189e1ee9e3fb2c3b019d2407e9f99
InventingWithMonster <mattgperry@gmail.com>
2021-03-11 19:13:43 +0100

    Adding missing files

---
+++ src/render/svg/config-motion.ts
@@ -0,0 +1,57 @@
+import { MotionComponentConfig } from "../../motion"
+import { SVGRenderState } from "./types"
+import { renderSVG, svgVisualElement } from "./visual-element"
+import { scrapeMotionValuesFromProps as scrapeSVGProps } from "./utils/scrape-motion-values"
+import { makeUseVisualState } from "../../motion/utils/use-visual-state"
+import { createSvgRenderState } from "./utils/create-render-state"
+import { buildSVGAttrs } from "./utils/build-attrs"
+
+export const svgMotionConfig: Partial<MotionComponentConfig<
+    SVGElement,
+    SVGRenderState
+>> = {
+    createVisualElement: (_, options) =>
+        svgVisualElement(options, { enableHardwareAcceleration: false }),
+    useVisualState: makeUseVisualState({
+        scrapeMotionValuesFromProps: scrapeSVGProps,
+        createRenderState: createSvgRenderState,
+        onMount: (props, instance, { renderState, latestValues }) => {
+            try {
+                renderState.dimensions =
+                    typeof (instance as SVGGraphicsElement).getBBox ===
+                    "function"
+                        ? (instance as SVGGraphicsElement).getBBox()
+                        : (instance.getBoundingClientRect() as DOMRect)
+            } catch (e) {
+                // Most likely trying to measure an unrendered element under Firefox
+                renderState.dimensions = {
+                    x: 0,
+                    y: 0,
+                    width: 0,
+                    height: 0,
+                }
+            }
+
+            if (isPath(instance)) {
+                renderState.totalPathLength = instance.getTotalLength()
+            }
+
+            buildSVGAttrs(
+                renderState,
+                latestValues,
+                undefined,
+                undefined,
+                { enableHardwareAcceleration: false },
+                props.transformTemplate
+            )
+
+            renderSVG(instance, renderState)
+        },
+    }),
+}
+
+function isPath(
+    element: SVGElement | SVGPathElement
+): element is SVGPathElement {
+    return element.tagName === "path"
+}
