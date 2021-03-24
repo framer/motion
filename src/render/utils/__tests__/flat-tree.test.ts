80880345260e31542603d40101080818a1e2bad7
InventingWithMonster <mattgperry@gmail.com>
2021-03-24 10:28:36 +0100

    Updating test

--- src/render/utils/__tests__/flat-tree.test.ts
+++ src/render/utils/__tests__/flat-tree.test.ts
@@ -4,13 +4,17 @@ import { FlatTree } from "../flat-tree"
 describe("FlatTree", () => {
     test("Correctly sorts by depth on iteration", () => {
         const tree = new FlatTree()
+
         tree.add({ depth: 1 })
         tree.add({ depth: 0 })
         expect((tree as any).children).toBe([{ depth: 1 }, { depth: 0 }])
 
         const received: WithDepth[] = []
         tree.forEach((child) => received.push(child))
-        expect(received).toBe([{ depth: 0 }, { depth: 1 }])
-        expect((tree as any).children).toBe([{ depth: 0 }, { depth: 1 }])
+        expect(received).toStrictEqual([{ depth: 0 }, { depth: 1 }])
+        expect((tree as any).children).toStrictEqual([
+            { depth: 0 },
+            { depth: 1 },
+        ])
     })
 })
