d7ea8eeaf750cb18f0c06fbc100123794b297f36
InventingWithMonster <mattgperry@gmail.com>
2021-04-13 13:55:35 +0200

    Latest

---
+++ src/render/dom/projection/relative-set.ts
@@ -0,0 +1,24 @@
+import { calcRelativeOffset } from "../../../motion/features/layout/utils"
+import { eachAxis } from "../../../utils/each-axis"
+import { VisualElement } from "../../types"
+
+export function setCurrentViewportBox(visualElement: VisualElement) {
+    const projectionParent = visualElement.getProjectionParent()
+
+    if (!projectionParent) {
+        visualElement.rebaseProjectionTarget()
+    } else {
+        const relativeOffset = calcRelativeOffset(
+            projectionParent.getLayoutState().layout,
+            visualElement.getLayoutState().layout
+        )
+        eachAxis((axis) => {
+            visualElement.setProjectionTargetAxis(
+                axis,
+                relativeOffset[axis].min,
+                relativeOffset[axis].max,
+                true
+            )
+        })
+    }
+}
---
+++ src/render/dom/projection/utils.ts
@@ -0,0 +1,96 @@
+import sync from "framesync"
+import { copyAxisBox } from "../../../utils/geometry"
+import { VisualElement } from "../../types"
+import { compareByDepth } from "../../utils/compare-by-depth"
+
+export function updateTreeLayoutMeasurements(visualElement: VisualElement) {
+    withoutTreeTransform(visualElement, () => {
+        const allChildren = collectProjectingChildren(visualElement)
+        batchResetAndMeasure(allChildren)
+
+        updateLayoutMeasurement(visualElement)
+    })
+
+    visualElement.rebaseProjectionTarget(
+        true,
+        visualElement.measureViewportBox(false)
+    )
+}
+
+export function collectProjectingChildren(
+    visualElement: VisualElement
+): VisualElement[] {
+    const children: VisualElement[] = []
+
+    const addChild = (child: VisualElement) => {
+        child.isProjecting() && children.push(child)
+        child.children.forEach(addChild)
+    }
+
+    visualElement.children.forEach(addChild)
+
+    return children.sort(compareByDepth)
+}
+
+/**
+ * Perform the callback after temporarily unapplying the transform
+ * upwards through the tree.
+ */
+export function withoutTreeTransform(
+    visualElement: VisualElement,
+    callback: () => void
+) {
+    const { parent } = visualElement
+    const { isEnabled } = visualElement.projection
+    isEnabled && visualElement.resetTransform()
+
+    parent ? withoutTreeTransform(parent, callback) : callback()
+
+    isEnabled && visualElement.restoreTransform()
+}
+
+/**
+ * Update the layoutState by measuring the DOM layout. This
+ * should be called after resetting any layout-affecting transforms.
+ */
+export function updateLayoutMeasurement(visualElement: VisualElement) {
+    const layoutState = visualElement.getLayoutState()
+
+    visualElement.notifyBeforeLayoutMeasure(layoutState.layout)
+
+    layoutState.isHydrated = true
+    layoutState.layout = visualElement.measureViewportBox()
+    layoutState.layoutCorrected = copyAxisBox(layoutState.layout)
+
+    visualElement.notifyLayoutMeasure(
+        layoutState.layout,
+        visualElement.prevViewportBox || layoutState.layout
+    )
+
+    sync.update(() => visualElement.rebaseProjectionTarget())
+}
+
+/**
+ * Record the viewport box as it was before an expected mutation/re-render
+ */
+export function snapshotViewportBox(visualElement: VisualElement) {
+    visualElement.prevViewportBox = visualElement.measureViewportBox(false)
+
+    /**
+     * Update targetBox to match the prevViewportBox. This is just to ensure
+     * that targetBox is affected by scroll in the same way as the measured box
+     */
+    visualElement.rebaseProjectionTarget(false, visualElement.prevViewportBox)
+}
+
+export function batchResetAndMeasure(order: VisualElement[]) {
+    /**
+     * Write: Reset any transforms on children elements so we can read their actual layout
+     */
+    order.forEach((child) => child.resetTransform())
+
+    /**
+     * Read: Measure the actual layout
+     */
+    order.forEach(updateLayoutMeasurement)
+}
