bc88003d53e2522f6bca1c5253d3ebc96d2642fc
InventingWithMonster <mattgperry@gmail.com>
2021-05-07 11:00:39 +0100

    Moving layout animations to batcher

--- src/components/AnimateSharedLayout/utils/batcher.ts
+++ src/components/AnimateSharedLayout/utils/batcher.ts
@@ -1,8 +1,12 @@
 import sync, { flushSync } from "framesync"
 import {
-    batchResetAndMeasure,
-    withoutTreeTransform,
+    collectProjectingAncestors,
+    updateLayoutMeasurement,
 } from "../../../render/dom/projection/utils"
+import {
+    batchLayout,
+    flushLayout,
+} from "../../../render/dom/utils/batch-layout"
 import { VisualElement } from "../../../render/types"
 import { compareByDepth } from "../../../render/utils/compare-by-depth"
 import { Presence, SyncLayoutBatcher, SyncLayoutLifecycles } from "../types"
@@ -23,51 +27,68 @@ export function createBatcher(): SyncLayoutBatcher {
     return {
         add: (child) => queue.add(child),
         flush: ({ layoutReady, parent } = defaultHandler) => {
-            const order = Array.from(queue).sort(compareByDepth)
+            batchLayout((read, write) => {
+                const order = Array.from(queue).sort(compareByDepth)
+                const ancestors = parent
+                    ? collectProjectingAncestors(parent)
+                    : []
 
-            if (parent) {
-                withoutTreeTransform(parent, () => {
-                    batchResetAndMeasure(order)
+                write(() => {
+                    const allElements = [...ancestors, ...order]
+                    allElements.forEach((element) => element.resetTransform())
                 })
-            } else {
-                batchResetAndMeasure(order)
-            }
 
-            /**
-             * Write: Notify the VisualElements they're ready for further write operations.
-             */
-            order.forEach(layoutReady)
+                read(() => {
+                    order.forEach(updateLayoutMeasurement)
+                })
 
-            /**
-             * After all children have started animating, ensure any Entering components are set to Present.
-             * If we add deferred animations (set up all animations and then start them in two loops) this
-             * could be moved to the start loop. But it needs to happen after all the animations configs
-             * are generated in AnimateSharedLayout as this relies on presence data
-             */
-            order.forEach((child) => {
-                if (child.isPresent) child.presence = Presence.Present
+                write(() => {
+                    ancestors.forEach((element) => element.restoreTransform())
+                    order.forEach(layoutReady)
+                })
+
+                read(() => {
+                    /**
+                     * After all children have started animating, ensure any Entering components are set to Present.
+                     * If we add deferred animations (set up all animations and then start them in two loops) this
+                     * could be moved to the start loop. But it needs to happen after all the animations configs
+                     * are generated in AnimateSharedLayout as this relies on presence data
+                     */
+                    order.forEach((child) => {
+                        if (child.isPresent) child.presence = Presence.Present
+                    })
+                })
+
+                write(() => {
+                    /**
+                     * Starting these animations will have queued jobs on the frame loop. In some situations,
+                     * like when removing an element, these will be processed too late after the DOM is manipulated,
+                     * leaving a flash of incorrectly-projected content. By manually flushing these jobs
+                     * we ensure there's no flash.
+                     */
+                    flushSync.preRender()
+                    flushSync.render()
+                })
+
+                read(() => {
+                    /**
+                     * Schedule a callback at the end of the following frame to assign the latest projection
+                     * box to the prevViewportBox snapshot. Once global batching is in place this could be run
+                     * synchronously. But for now it ensures that if any nested `AnimateSharedLayout` top-level
+                     * child attempts to calculate its previous relative position against a prevViewportBox
+                     * it will be against its latest projection box instead, as the snapshot is useless beyond this
+                     * render.
+                     */
+                    sync.postRender(() =>
+                        order.forEach(assignProjectionToSnapshot)
+                    )
+
+                    queue.clear()
+                })
             })
 
-            /**
-             * Starting these animations will have queued jobs on the frame loop. In some situations,
-             * like when removing an element, these will be processed too late after the DOM is manipulated,
-             * leaving a flash of incorrectly-projected content. By manually flushing these jobs
-             * we ensure there's no flash.
-             */
-            flushSync.preRender()
-            flushSync.render()
-
-            /**
-             * Schedule a callback at the end of the following frame to assign the latest projection
-             * box to the prevViewportBox snapshot. Once global batching is in place this could be run
-             * synchronously. But for now it ensures that if any nested `AnimateSharedLayout` top-level
-             * child attempts to calculate its previous relative position against a prevViewportBox
-             * it will be against its latest projection box instead, as the snapshot is useless beyond this
-             * render.
-             */
-            sync.postRender(() => order.forEach(assignProjectionToSnapshot))
-
-            queue.clear()
+            // TODO: Need to find a layout-synchronous way of flushing this
+            flushLayout()
         },
     }
 }
