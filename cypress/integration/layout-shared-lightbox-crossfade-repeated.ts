bf0fdb4d095545c6a3cc187434760bba169f64ef
Rijk van Wel <rijk@uniti.be>
2021-06-18 13:31:36 +0200

    Add test with realistic crossfade and switch to different item

--- cypress/integration/layout-shared-lightbox-crossfade-repeated.ts
+++ cypress/integration/layout-shared-lightbox-crossfade-repeated.ts
@@ -16,7 +16,7 @@ function expectBbox(element: HTMLElement, expectedBbox: BoundingBox) {
 }
 
 describe("AnimateSharedLayout lightbox example, toggle back and forth", () => {
-    it("Correctly animates back and forth with crossfade", () => {
+    it("Correctly animates back and forth with a very quick crossfade", () => {
         function open(session: Cypress.cy) {
             return session
                 .get("#item-parent")
@@ -160,6 +160,97 @@ describe("AnimateSharedLayout lightbox example, toggle back and forth", () => {
             cy
                 .visit(
                     `?test=layout-shared-lightbox-crossfade&type=crossfade&partial-ease=true`
+                )
+                .wait(50)
+        )
+    })
+    it("Correctly animates back and forth with a normal crossfade", () => {
+        const beCrossfading = ([$box]: any) => {
+            const { borderRadius, opacity } = window.getComputedStyle($box)
+            expect(borderRadius).not.to.equal("0%")
+            expect(borderRadius).not.to.equal("8.33333% / 12.5%")
+            expect(opacity).to.be.greaterThan(0)
+
+            // Fade out has a delay; see easeCrossfadeOut()
+            // expect(opacity).to.be.lessThan(1)
+
+            const { width, height } = $box.getBoundingClientRect()
+            expect(width).to.be.greaterThan(180)
+            expect(width).to.be.lessThan(600)
+            expect(height).to.be.lessThan(580)
+            expect(height).to.be.greaterThan(400)
+        }
+
+        const beCircle = ([$box]: any) => {
+            const { borderRadius, opacity } = window.getComputedStyle($box)
+            expect(opacity).to.equal("0.5")
+            expect(borderRadius).to.equal("50%")
+
+            const { width, height } = $box.getBoundingClientRect()
+            expect(width).to.equal(50)
+            expect(height).to.equal(50)
+        }
+
+        const beOpen = ([$box]: any) => {
+            const { borderRadius, opacity } = window.getComputedStyle($box)
+            expect(borderRadius).to.equal("8.33333% / 12.5%")
+            expect(opacity).to.equal("1")
+
+            const { width, height } = $box.getBoundingClientRect()
+            expect(width).to.equal(600)
+            expect(height).to.equal(400)
+        }
+
+        const beInvisible = ([$box]: any) => {
+            const { opacity } = window.getComputedStyle($box)
+            expect(opacity).to.equal("0")
+        }
+
+        function open(num: number, session: Cypress.cy) {
+            return session
+                .get(`li:nth-child(${num})`)
+                .trigger("click")
+                .should(beCrossfading)
+                .get(`li:nth-child(${num}) > div`)
+                .should(beCircle)
+                .get("#parent")
+                .should(beCrossfading)
+                .get("#child")
+                .should(beCircle)
+                .wait(2000)
+                .get("#parent")
+                .should(beOpen)
+                .get(`li:nth-child(${num})`)
+                .should(beInvisible)
+        }
+
+        function close(num: number, session: Cypress.cy) {
+            return session
+                .get("#overlay")
+                .trigger("click")
+                .get(`li:nth-child(${num})`)
+                .should(beCrossfading)
+                .get(`li:nth-child(${num}) > div`)
+                .should(beCircle)
+                .get("#parent")
+                .should(beCrossfading)
+                .get("#child")
+                .should(beCircle)
+        }
+
+        pipe(
+            (session: Cypress.cy) => open(2, session),
+            (session: Cypress.cy) => close(2, session),
+            (session: Cypress.cy) => session.wait(1000),
+            (session: Cypress.cy) => open(2, session),
+            (session: Cypress.cy) => close(2, session),
+            (session: Cypress.cy) => session.wait(1000),
+            (session: Cypress.cy) => open(3, session),
+            (session: Cypress.cy) => close(3, session)
+        )(
+            cy
+                .visit(
+                    `?test=layout-shared-lightbox-crossfade&type=crossfade&duration=3`
                 )
                 .wait(50)
         )
