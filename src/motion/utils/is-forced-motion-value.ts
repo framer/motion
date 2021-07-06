f365aa3582a39da315ff38c202e8ce39989c7246
Shuang Qiu <shuang@framer.com>
2021-07-06 18:15:38 +0200

    fix scale correction

--- src/motion/utils/is-forced-motion-value.ts
+++ src/motion/utils/is-forced-motion-value.ts
@@ -1,14 +1,18 @@
+import { scaleCorrectors } from "../../projection/styles/scale-correction"
 import { MotionProps } from "../.."
-// import { valueScaleCorrection } from "../../render/dom/projection/scale-correction"
 import {
     isTransformOriginProp,
     isTransformProp,
 } from "../../render/html/utils/transform"
 
-export function isForcedMotionValue(key: string, _props: MotionProps) {
+export function isForcedMotionValue(
+    key: string,
+    { layout, layoutId }: MotionProps
+) {
     return (
-        isTransformProp(key) || isTransformOriginProp(key) // ||
-        // ((layout || layoutId !== undefined) &&
-        //     (!!valueScaleCorrection[key] || key === "opacity"))
+        isTransformProp(key) ||
+        isTransformOriginProp(key) ||
+        ((layout || layoutId !== undefined) &&
+            (!!scaleCorrectors[key] || key === "opacity"))
     )
 }
