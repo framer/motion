3a2f18177eba1190d6f5e2e090a84ad08db491a1
Shuang Qiu <shuang@framer.com>
2021-07-09 12:22:46 +0200

    Don't match values like "0.5"

--- src/utils/__tests__/is-zero-value-string.test.ts
+++ src/utils/__tests__/is-zero-value-string.test.ts
@@ -4,7 +4,7 @@ describe("isZeroValueString", () => {
     test("should correctly identify numerical strings", () => {
         expect(isZeroValueString("0px")).toBe(true)
         expect(isZeroValueString("0%")).toBe(true)
-        expect(isZeroValueString("0.5%")).toBe(true)
+        expect(isZeroValueString("0.5%")).toBe(false)
         expect(isZeroValueString("10.1%")).toBe(false)
         expect(isZeroValueString("0")).toBe(false)
         expect(isZeroValueString("10.1")).toBe(false)
