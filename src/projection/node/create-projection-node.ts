820fb2c6bd9ec983828beb46a043b3d8eedca2a3
Shuang Qiu <shuang@framer.com>
2021-07-29 16:39:06 +0200

    force layout animation if animating from another element

--- src/projection/node/create-projection-node.ts
+++ src/projection/node/create-projection-node.ts
@@ -159,7 +159,10 @@ export function createProjectionNode<I>({
                             !boxEquals(this.targetLayout, newLayout)
 
                         if (
-                            hasLayoutChanged &&
+                            // If the element is animating from another element,
+                            // we should force the animation even if they have
+                            // the same layout
+                            (hasLayoutChanged || this.resumeFrom) &&
                             // If there's a current animation and the target hasn't changed, ignore
                             (targetChanged || !this.currentAnimation)
                         ) {
