a79eebbdaf33725a6e891f8f073a85c34b1e7d23
Niels van Hoorn <niels@framer.com>
2021-08-18 14:32:44 +0200

    Move LayoutGroup tests from FramerStudio

---
+++ src/components/LayoutGroup/__tests__/LayoutGroup.test.tsx
@@ -0,0 +1,65 @@
+import { render } from "@testing-library/react"
+import * as React from "react"
+import { LayoutGroup } from "../index"
+import { LayoutGroupContext } from "../../../context/LayoutGroupContext"
+
+const Consumer = ({ id = "1" }: any) => {
+    const value = React.useContext(LayoutGroupContext)
+    return <div data-testid={id}>{value.id}</div>
+}
+it("if it's the first LayoutGroup it sets the group id", () => {
+    const tree = (
+        <LayoutGroup id={"a"}>
+            <Consumer />
+        </LayoutGroup>
+    )
+    console.log(LayoutGroup, Consumer)
+
+    const { getByTestId } = render(tree)
+
+    expect(getByTestId("1").textContent).toBe("a")
+})
+
+it("if it's a nested LayoutGroup it appends to the group id", () => {
+    const tree = (
+        <LayoutGroup id={"a"}>
+            <LayoutGroup id={"b"}>
+                <Consumer />
+            </LayoutGroup>
+        </LayoutGroup>
+    )
+
+    const { getByTestId } = render(tree)
+
+    expect(getByTestId("1").textContent).toBe("a-b")
+})
+
+it("if the value of id is undefined, it doesn't change the group id", () => {
+    const tree = (
+        <LayoutGroup id={"a"}>
+            <LayoutGroup id={undefined}>
+                <Consumer />
+            </LayoutGroup>
+        </LayoutGroup>
+    )
+
+    const { getByTestId } = render(tree)
+
+    expect(getByTestId("1").textContent).toBe("a")
+})
+
+it("if the parent group id is undefined, child LayoutGroups still append the group id", () => {
+    const tree = (
+        <LayoutGroup id={"a"}>
+            <LayoutGroup id={undefined}>
+                <LayoutGroup id={"b"}>
+                    <Consumer />
+                </LayoutGroup>
+            </LayoutGroup>
+        </LayoutGroup>
+    )
+
+    const { getByTestId } = render(tree)
+
+    expect(getByTestId("1").textContent).toBe("a-b")
+})
