230d9ed748149d17c43de974de489c55a8028983
Shuang Qiu <shuang@framer.com>
2021-08-23 17:54:47 +0200

    take startTransition out of useInstantLayoutTransition

--- src/projection/use-instant-layout-transition.ts
+++ src/projection/use-instant-layout-transition.ts
@@ -1,12 +1,14 @@
-import * as React from "react"
 import { rootProjectionNode } from "./node/HTMLProjectionNode"
 
-export function useInstantLayoutTransition() {
-    const startTransition = React.useCallback((cb?: () => void) => {
-        if (!rootProjectionNode.current) return
-        rootProjectionNode.current.isUpdating = false
-        rootProjectionNode.current.blockUpdate()
-        cb?.()
-    }, [])
+export function useInstantLayoutTransition(): (
+    cb?: (() => void) | undefined
+) => void {
     return startTransition
+}
+
+function startTransition(cb?: () => void) {
+    if (!rootProjectionNode.current) return
+    rootProjectionNode.current.isUpdating = false
+    rootProjectionNode.current.blockUpdate()
+    cb?.()
 }
