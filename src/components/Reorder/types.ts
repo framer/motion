27e423e8b0042c3df4b934f7e711cef2955500bd
InventingWithMonster <mattgperry@gmail.com>
2021-09-09 15:17:43 +0100

    Latest

--- src/components/Reorder/types.ts
+++ src/components/Reorder/types.ts
@@ -6,16 +6,6 @@ export interface ReorderContextProps<T> {
     updateOrder: (id: T, offset: number, velocity: number) => void
 }
 
-export interface RenderGroupProps<T> {
-    as?: string
-    axis: "x" | "y"
-    onReorder: (order: T[]) => void
-}
-
-export interface RenderTriggerProps {
-    as?: string
-}
-
 export interface ItemData<T> {
     value: T
     layout: Axis
