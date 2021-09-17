8e857aec3e5ff164d01f678ca21c3b503404f919
InventingWithMonster <mattgperry@gmail.com>
2021-09-17 11:55:31 +0200

    Removing logs

--- src/projection/use-instant-layout-transition.ts
+++ src/projection/use-instant-layout-transition.ts
@@ -3,12 +3,10 @@ import { rootProjectionNode } from "./node/HTMLProjectionNode"
 export function useInstantLayoutTransition(): (
     cb?: (() => void) | undefined
 ) => void {
-    console.log("render")
     return startTransition
 }
 
 function startTransition(cb?: () => void) {
-    console.log("starting transition, blocking update")
     if (!rootProjectionNode.current) return
     rootProjectionNode.current.isUpdating = false
     rootProjectionNode.current.blockUpdate()
