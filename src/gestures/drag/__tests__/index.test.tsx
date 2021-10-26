b2f1e4027de9c3bf2de09259f066eb2ece8aa84b
InventingWithMonster <mattgperry@gmail.com>
2021-10-26 11:17:59 +0200

    Amending tests

--- src/gestures/drag/__tests__/index.test.tsx
+++ src/gestures/drag/__tests__/index.test.tsx
@@ -147,7 +147,7 @@ describe("dragging", () => {
         pointer.end() // + 3 = 14
         await onDragEnd.promise
 
-        expect(count).toBe(14)
+        expect(count).toBe(12)
     })
 
     test("dragEnd returns transformed pointer", async () => {
