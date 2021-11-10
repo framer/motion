792ace5fd4796dd8f49dc81b6f24e2ef5f9c8484
Matt Perry <mattgperry@gmail.com>
2021-11-10 15:19:39 +0100

    Making test more permissive

--- cypress/integration/unit-conversion.ts
+++ cypress/integration/unit-conversion.ts
@@ -1,10 +1,10 @@
 describe("Unit conversion", () => {
     it("Animate x from 0 to calc", () => {
         cy.visit("?test=unit-conversion")
-            .wait(50)
+            .wait(100)
             .get("#box")
             .trigger("click")
-            .wait(30)
+            .wait(100)
             .should(([$box]: any) => {
                 const { left } = $box.getBoundingClientRect()
                 expect(left).to.equal(150)
