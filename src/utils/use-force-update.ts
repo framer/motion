709343124478b3ebad62f6291c040cb3e48f3508
Matt Perry <mattgperry@gmail.com>
2021-11-22 14:29:10 +0000

    Improving variable name

--- src/utils/use-force-update.ts
+++ src/utils/use-force-update.ts
@@ -3,16 +3,16 @@ import { useState, useCallback, useRef } from "react"
 import { useUnmountEffect } from "./use-unmount-effect"
 
 export function useForceUpdate(): [VoidFunction, number] {
-    const unloadingRef = useRef(false)
+    const isUnmountingRef = useRef(false)
     const [forcedRenderCount, setForcedRenderCount] = useState(0)
-    useUnmountEffect(() => (unloadingRef.current = true))
+    useUnmountEffect(() => (isUnmountingRef.current = true))
 
     const forceRender = useCallback(() => {
-        !unloadingRef.current && setForcedRenderCount(forcedRenderCount + 1)
+        !isUnmountingRef.current && setForcedRenderCount(forcedRenderCount + 1)
     }, [forcedRenderCount])
 
     /**
-     * Defer this to  the end of the next animation frame in case there are multiple
+     * Defer this to the end of the next animation frame in case there are multiple
      * synchronous calls.
      */
     const deferredForceRender = useCallback(
