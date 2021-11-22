255c939342cc88d683644b608ddd2fa9a63ea68c
Matt Perry <mattgperry@gmail.com>
2021-11-22 12:05:45 +0000

    Batching forceRender calls

--- src/utils/use-force-update.ts
+++ src/utils/use-force-update.ts
@@ -1,3 +1,4 @@
+import sync from "framesync"
 import { useState, useCallback, useRef } from "react"
 import { useUnmountEffect } from "./use-unmount-effect"
 
@@ -6,10 +7,18 @@ export function useForceUpdate(): [VoidFunction, number] {
     const [forcedRenderCount, setForcedRenderCount] = useState(0)
     useUnmountEffect(() => (unloadingRef.current = true))
 
-    return [
-        useCallback(() => {
-            !unloadingRef.current && setForcedRenderCount(forcedRenderCount + 1)
-        }, [forcedRenderCount]),
-        forcedRenderCount,
-    ]
+    const forceRender = useCallback(() => {
+        !unloadingRef.current && setForcedRenderCount(forcedRenderCount + 1)
+    }, [forcedRenderCount])
+
+    /**
+     * Defer this to  the end of the next animation frame in case there are multiple
+     * synchronous calls.
+     */
+    const deferredForceRender = useCallback(
+        () => sync.postRender(forceRender),
+        [forceRender]
+    )
+
+    return [deferredForceRender, forcedRenderCount]
 }
