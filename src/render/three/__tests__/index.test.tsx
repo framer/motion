304b6afd59eb8e6de7710caf1eb0153d6eb7d39a
Matt Perry <mattgperry@gmail.com>
2021-12-02 16:50:25 +0100

    Adding failing tests for motion value injection

--- src/render/three/__tests__/index.test.tsx
+++ src/render/three/__tests__/index.test.tsx
@@ -1,9 +1,12 @@
 import * as React from "react"
-import { Canvas } from "@react-three/fiber"
+import { useEffect, useRef } from "react"
+import { Euler, Vector3 } from "three"
+import { Canvas, Object3DNode } from "@react-three/fiber"
 import ReactThreeTestRenderer from "@react-three/test-renderer"
 import { render } from "../../../../jest.setup"
 import { motion } from "../motion"
 import { ResolvedValues } from "../../types"
+import { MotionValue, motionValue } from "../../../value"
 
 jest.mock("scheduler", () => require("scheduler/unstable_mock"))
 
@@ -59,5 +62,144 @@ describe("motion for three", () => {
             rotateY: 5,
             rotateZ: 6,
         })
+    })
+
+    test("Reads initial value from scale as number", async () => {
+        const result = await new Promise<any[]>(async (resolve) => {
+            const output: ResolvedValues[] = []
+
+            function Component() {
+                return (
+                    <motion.mesh
+                        animate={{ scale: 2 }}
+                        scale={5}
+                        onUpdate={(latest) => output.push({ ...latest })}
+                        onAnimationComplete={() => resolve(output)}
+                        transition={{
+                            duration: 0.1,
+                            ease: (t) => (t < 0.5 ? t : 0),
+                        }}
+                    />
+                )
+            }
+
+            ReactThreeTestRenderer.create(<Component />)
+        })
+
+        const lastFrame = result[result.length - 1]
+        expect(lastFrame).toEqual({
+            scale: 5,
+        })
+    })
+
+    test("Reads initial value from drilled props", async () => {
+        const result = await new Promise<any[]>(async (resolve) => {
+            const output: ResolvedValues[] = []
+
+            function Component() {
+                return (
+                    <motion.mesh
+                        animate={{
+                            scaleX: 1,
+                            scaleY: 2,
+                            scaleZ: 3,
+                            x: 100,
+                            y: 200,
+                            z: 300,
+                            rotateX: 100,
+                            rotateY: 200,
+                            rotateZ: 300,
+                        }}
+                        position-x={1}
+                        position-y={2}
+                        position-z={3}
+                        rotation-x={4}
+                        rotation-y={5}
+                        rotation-z={6}
+                        scale-x={7}
+                        scale-y={8}
+                        scale-z={9}
+                        onUpdate={(latest) => output.push({ ...latest })}
+                        onAnimationComplete={() => resolve(output)}
+                        transition={{
+                            duration: 0.1,
+                            ease: (t) => (t < 0.5 ? t : 0),
+                        }}
+                    />
+                )
+            }
+
+            ReactThreeTestRenderer.create(<Component />)
+        })
+
+        const lastFrame = result[result.length - 1]
+        expect(lastFrame).toEqual({
+            x: 1,
+            y: 2,
+            z: 3,
+            rotateX: 4,
+            rotateY: 5,
+            rotateZ: 6,
+            scaleX: 7,
+            scaleY: 8,
+            scaleZ: 9,
+        })
+    })
+
+    test("Accepts motion values", async () => {
+        const result = await new Promise<Object3DNode<any, any>>((resolve) => {
+            const x = motionValue(1)
+            const scale = motionValue(2)
+            const rotateX = motionValue(3)
+            function Component() {
+                const ref = useRef(null)
+                useEffect(() => {
+                    resolve(ref.current as any)
+                })
+                return (
+                    <motion.mesh
+                        ref={ref}
+                        position-x={x}
+                        scale={scale}
+                        rotation={[rotateX, 0, 0]}
+                        onAnimationComplete={() => resolve([x, scale, rotateX])}
+                        transition={{ duration: 0.1 }}
+                    />
+                )
+            }
+
+            ReactThreeTestRenderer.create(<Component />)
+        })
+
+        expect((result.position as Vector3).x).toEqual(1)
+        expect(result.scale).toEqual(2)
+        expect((result.rotation as Euler).x).toEqual(3)
+    })
+
+    test("Animates motion values", async () => {
+        const result = await new Promise<MotionValue<number>[]>((resolve) => {
+            const x = motionValue(0)
+            const scale = motionValue(0)
+            const rotateX = motionValue(0)
+            function Component() {
+                return (
+                    <motion.mesh
+                        position-x={x}
+                        scale={scale}
+                        rotation={[rotateX, 0, 0]}
+                        animate={{ x: 100, scale: 5, rotateX: 90 }}
+                        onAnimationComplete={() => resolve([x, scale, rotateX])}
+                        transition={{ duration: 0.1 }}
+                    />
+                )
+            }
+
+            ReactThreeTestRenderer.create(<Component />)
+        })
+
+        const [a, b, c] = result
+        expect(a.get()).toEqual(100)
+        expect(b.get()).toEqual(5)
+        expect(c.get()).toEqual(90)
     })
 })
--- src/render/three/types.ts
+++ src/render/three/types.ts
@@ -1,10 +1,10 @@
-import { MeshProps } from "@react-three/fiber"
+import { Color, Euler, MeshProps, Vector3 } from "@react-three/fiber"
 import type {
     ForwardRefExoticComponent,
     PropsWithoutRef,
     RefAttributes,
 } from "react"
-import { HoverHandlers, TapHandlers } from "../.."
+import { HoverHandlers, MotionValue, TapHandlers } from "../.."
 import { AnimationProps } from "../../motion/types"
 import { ResolvedValues } from "../types"
 import { AnimationLifecycles } from "../utils/lifecycles"
@@ -28,6 +28,23 @@ export type ForwardRefComponent<T, P> = ForwardRefExoticComponent<
     PropsWithoutRef<P> & RefAttributes<T>
 >
 
+type MotionValueOrNumber = number | MotionValue<number>
+type MotionValueVector3 = [
+    MotionValueOrNumber,
+    MotionValueOrNumber,
+    MotionValueOrNumber
+]
+
+export type AcceptMotionValues<T> = Omit<
+    T,
+    "position" | "scale" | "rotation" | "color"
+> & {
+    position?: Vector3 | MotionValueVector3 | MotionValueOrNumber
+    scale?: Vector3 | MotionValueVector3 | MotionValueOrNumber
+    rotation?: Euler | MotionValueVector3 | MotionValueOrNumber
+    color?: Color | MotionValue<string>
+}
+
 /**
  * Motion-optimised versions of React's HTML components.
  *
@@ -36,6 +53,7 @@ export type ForwardRefComponent<T, P> = ForwardRefExoticComponent<
 export type ThreeMotionComponents = {
     [K in keyof JSX.IntrinsicElements]: ForwardRefComponent<
         JSX.IntrinsicElements[K],
-        ThreeMotionProps & Omit<JSX.IntrinsicElements[K], "onUpdate">
+        ThreeMotionProps &
+            Omit<AcceptMotionValues<JSX.IntrinsicElements[K]>, "onUpdate">
     >
 }
