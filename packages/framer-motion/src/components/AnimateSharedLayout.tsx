ad1932b19da6b2b181209aac2ca7cad6d8665dd9
Hyeok_E <61281239+HyeokE@users.noreply.github.com>
2022-04-15 17:06:39 +0900

    Update AnimateSharedLayout.tsx

--- packages/framer-motion/src/components/AnimateSharedLayout.tsx
+++ packages/framer-motion/src/components/AnimateSharedLayout.tsx
@@ -3,6 +3,6 @@ import { useConstant } from "../utils/use-constant"
 import { LayoutGroup } from "./LayoutGroup"
 
 let id = 0
-export const AnimateSharedLayout: React.FunctionComponent = ({ children }) => (
+export const AnimateSharedLayout: React.FunctionComponent<React.PropsWithChildren> = ({ children }) => (
     <LayoutGroup id={useConstant(() => `asl-${id++}`)}>{children}</LayoutGroup>
 )
