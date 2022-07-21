30e3f2d9fe28a9d13ba2f61f7a5e645c4789417f
alexandredev3 <costasalexandre511@gmail.com>
2022-07-21 14:48:50 +0000

    adding deprecated tag to use-viewport-scroll and use-element-scroll

--- packages/framer-motion/src/value/scroll/use-element-scroll.ts
+++ packages/framer-motion/src/value/scroll/use-element-scroll.ts
@@ -2,6 +2,9 @@ import { RefObject } from "react"
 import { warnOnce } from "../../utils/warn-once"
 import { useScroll } from "../use-scroll"
 
+/**
+ * @deprecated useElementScroll is deprecated. Convert to useScroll({ container: ref })
+ */
 export function useElementScroll(ref: RefObject<HTMLElement>) {
     warnOnce(
         false,
--- packages/framer-motion/src/value/scroll/use-viewport-scroll.ts
+++ packages/framer-motion/src/value/scroll/use-viewport-scroll.ts
@@ -1,6 +1,9 @@
 import { warnOnce } from "../../utils/warn-once"
 import { useScroll } from "../use-scroll"
 
+/**
+ * @deprecated useViewportScroll is deprecated. Convert to useScroll()
+ */
 export function useViewportScroll() {
     warnOnce(false, "useViewportScroll is deprecated. Convert to useScroll().")
     return useScroll()
