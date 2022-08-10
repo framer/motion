03a6fc92d126f1a58690dbce307f946713e21036
Matt Perry <mattgperry@gmail.com>
2022-08-10 17:16:08 +0200

    Removing boolean returns from array utils

--- packages/framer-motion/src/utils/array.ts
+++ packages/framer-motion/src/utils/array.ts
@@ -1,16 +1,10 @@
 export function addUniqueItem<T>(arr: T[], item: T) {
-    if (arr.indexOf(item) === -1) {
-        arr.push(item)
-        return true
-    }
+    if (arr.indexOf(item) === -1) arr.push(item)
 }
 
 export function removeItem<T>(arr: T[], item: T) {
     const index = arr.indexOf(item)
-    if (index > -1) {
-        arr.splice(index, 1)
-        return true
-    }
+    if (index > -1) arr.splice(index, 1)
 }
 
 // Adapted from array-move
