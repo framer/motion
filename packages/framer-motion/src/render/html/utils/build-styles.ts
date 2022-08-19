37b4311f7134f8013d066e1cf0f7c5418949be51
Matt Perry <mattgperry@gmail.com>
2022-08-19 13:45:36 +0200

    Size reductions

--- packages/framer-motion/src/render/html/utils/build-styles.ts
+++ packages/framer-motion/src/render/html/utils/build-styles.ts
@@ -2,7 +2,7 @@ import { MotionProps } from "../../../motion/types"
 import { HTMLRenderState } from "../types"
 import { ResolvedValues } from "../../types"
 import { DOMVisualElementOptions } from "../../dom/types"
-import { buildTransform, buildTransformOrigin } from "./build-transform"
+import { buildTransform } from "./build-transform"
 import { isCSSVariable } from "../../dom/utils/is-css-variable"
 import { transformProps } from "./transform"
 import { getValueAsType } from "../../dom/value-types/get-as-type"
@@ -16,9 +16,6 @@ export function buildHTMLStyles(
 ) {
     const { style, vars, transform, transformKeys, transformOrigin } = state
 
-    // Empty the transformKeys array. As we're throwing out refs to its items
-    // this might not be as cheap as suspected. Maybe using the array as a buffer
-    // with a manual incrementation would be better.
     transformKeys.length = 0
 
     // Track whether we encounter any transform or transformOrigin values.
@@ -59,31 +56,38 @@ export function buildHTMLStyles(
             if (!transformIsNone) continue
 
             // Otherwise check to see if this is a default transform
-            if (value !== (valueType.default ?? 0)) transformIsNone = false
+            if (value !== (valueType.default || 0)) transformIsNone = false
         } else if (key.startsWith("origin")) {
-            transformOrigin[key] = valueAsType
-
             // If this is a transform origin, flag and enable further transform-origin processing
             hasTransformOrigin = true
+
+            transformOrigin[key] = valueAsType
         } else {
             style[key] = valueAsType
         }
     }
 
-    if (hasTransform) {
+    if (hasTransform || transformTemplate) {
         style.transform = buildTransform(
             state,
             options,
             transformIsNone,
             transformTemplate
         )
-    } else if (transformTemplate) {
-        style.transform = transformTemplate({}, "")
     } else if (!latestValues.transform && style.transform) {
+        /**
+         * If we have previously created a transform but currently don't have any,
+         * reset transform style to none.
+         */
         style.transform = "none"
     }
 
     if (hasTransformOrigin) {
-        style.transformOrigin = buildTransformOrigin(transformOrigin)
+        const {
+            originX = "50%",
+            originY = "50%",
+            originZ = 0,
+        } = transformOrigin
+        style.transformOrigin = `${originX} ${originY} ${originZ}`
     }
 }
