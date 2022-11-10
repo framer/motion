0084d445ac274766101a65a0f0dc72735b837f22
Matt Perry <mattgperry@gmail.com>
2022-11-10 14:10:22 +0000

    Improving framerate by up to 20%

--- packages/framer-motion/src/projection/styles/__tests__/transform.test.ts
+++ packages/framer-motion/src/projection/styles/__tests__/transform.test.ts
@@ -2,11 +2,13 @@ import { buildProjectionTransform } from "../transform"
 import { createDelta } from "../../geometry/models"
 
 describe("buildProjectionTransform", () => {
-    it("Creates the expected transform for the provided arguments", () => {
+    it("Returns 'none' when no transform required", () => {
         expect(buildProjectionTransform(createDelta(), { x: 1, y: 1 })).toEqual(
             "none"
         )
+    })
 
+    it("Creates the expected transform for the provided arguments", () => {
         const delta = {
             x: {
                 translate: 100,
@@ -22,7 +24,7 @@ describe("buildProjectionTransform", () => {
             },
         }
         expect(buildProjectionTransform(delta, { x: 1, y: 1 })).toEqual(
-            "translate3d(100px, 300px, 0) scale(1, 1) scale(2, 4)"
+            "translate3d(100px, 300px, 0) scale(2, 4)"
         )
 
         expect(buildProjectionTransform(delta, { x: 2, y: 0.5 })).toEqual(
@@ -34,5 +36,47 @@ describe("buildProjectionTransform", () => {
         ).toEqual(
             "translate3d(50px, 600px, 0) scale(0.5, 2) rotate(45deg) scale(4, 2)"
         )
+    })
+
+    it("Doesn't apply unneccessary translation", () => {
+        const delta = {
+            x: {
+                translate: 0,
+                scale: 2,
+                origin: 0.5,
+                originPoint: 100,
+            },
+            y: {
+                translate: 0,
+                scale: 4,
+                origin: 0.5,
+                originPoint: 100,
+            },
+        }
+
+        expect(
+            buildProjectionTransform(delta, { x: 2, y: 4 }, { rotate: 10 })
+        ).toEqual("scale(0.5, 0.25) rotate(10deg) scale(4, 16)")
+    })
+
+    it("Doesn't apply unneccessary tree scale", () => {
+        const delta = {
+            x: {
+                translate: 100,
+                scale: 2,
+                origin: 0.5,
+                originPoint: 100,
+            },
+            y: {
+                translate: 100,
+                scale: 4,
+                origin: 0.5,
+                originPoint: 100,
+            },
+        }
+
+        expect(
+            buildProjectionTransform(delta, { x: 1, y: 1 }, { rotate: 10 })
+        ).toEqual("translate3d(100px, 100px, 0) rotate(10deg) scale(2, 4)")
     })
 })
--- packages/framer-motion/src/projection/styles/transform.ts
+++ packages/framer-motion/src/projection/styles/transform.ts
@@ -1,14 +1,13 @@
 import { ResolvedValues } from "../../render/types"
 import { Delta, Point } from "../geometry/types"
 
-export const identityProjection =
-    "translate3d(0px, 0px, 0) scale(1, 1) scale(1, 1)"
-
 export function buildProjectionTransform(
     delta: Delta,
     treeScale: Point,
     latestTransform?: ResolvedValues
 ): string {
+    let transform = ""
+
     /**
      * The translations we use to calculate are always relative to the viewport coordinate space.
      * But when we apply scales, we also scale the coordinate space of an element and its children.
@@ -17,13 +16,19 @@ export function buildProjectionTransform(
      */
     const xTranslate = delta.x.translate / treeScale.x
     const yTranslate = delta.y.translate / treeScale.y
-    let transform = `translate3d(${xTranslate}px, ${yTranslate}px, 0) `
+    if (xTranslate || yTranslate) {
+        transform = `translate3d(${xTranslate}px, ${yTranslate}px, 0) `
+    }
 
     /**
      * Apply scale correction for the tree transform.
      * This will apply scale to the screen-orientated axes.
      */
-    transform += `scale(${1 / treeScale.x}, ${1 / treeScale.y}) `
+    const treeScaleCorrectionX = 1 / treeScale.x
+    const treeScaleCorrectionY = 1 / treeScale.y
+    if (treeScaleCorrectionX !== 1 || treeScaleCorrectionY !== 1) {
+        transform += `scale(${treeScaleCorrectionX}, ${treeScaleCorrectionY}) `
+    }
 
     if (latestTransform) {
         const { rotate, rotateX, rotateY } = latestTransform
@@ -38,7 +43,9 @@ export function buildProjectionTransform(
      */
     const elementScaleX = delta.x.scale * treeScale.x
     const elementScaleY = delta.y.scale * treeScale.y
-    transform += `scale(${elementScaleX}, ${elementScaleY})`
+    if (elementScaleX !== 1 || elementScaleY !== 1) {
+        transform += `scale(${elementScaleX}, ${elementScaleY})`
+    }
 
-    return transform === identityProjection ? "none" : transform
+    return transform || "none"
 }
