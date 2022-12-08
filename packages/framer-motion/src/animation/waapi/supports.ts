642faa08a5f137b70a803df4723de9253441ef07
Matt Perry <mattgperry@gmail.com>
2022-12-08 15:33:34 +0100

    Updating comment

--- packages/framer-motion/src/animation/waapi/supports.ts
+++ packages/framer-motion/src/animation/waapi/supports.ts
@@ -8,6 +8,9 @@ const results = {}
 
 export const supports = {} as FeatureTests
 
+/**
+ * Generate features tests that cache their results.
+ */
 for (const key in featureTests) {
     supports[key] = () => {
         if (results[key] === undefined) results[key] = featureTests[key]()
