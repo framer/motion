fe22168978188376182707e6cae18bf8c9e4803b
Matt Perry <mattgperry@gmail.com>
2022-12-12 17:55:39 +0100

    Adding comment

--- packages/framer-motion/src/animation/optimized-appear/types.ts
+++ packages/framer-motion/src/animation/optimized-appear/types.ts
@@ -1,5 +1,9 @@
 export type MotionAppearAnimations = Map<string, Animation>
 
+/**
+ * The window global object acts as a bridge between our inline script
+ * triggering the optimized appear animations, and Framer Motion.
+ */
 declare global {
     interface Window {
         MotionAppearAnimations?: MotionAppearAnimations
