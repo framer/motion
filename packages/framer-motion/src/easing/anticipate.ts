111a8b8015870e21f42958d21badce999be66035
Matt Perry <mattgperry@gmail.com>
2022-12-14 16:53:59 +0100

    Adding support for back/circ easings to WAAPI

--- packages/framer-motion/src/easing/anticipate.ts
+++ packages/framer-motion/src/easing/anticipate.ts
@@ -1,12 +1,4 @@
-import { createBackIn } from "./back"
-import { EasingFunction } from "./types"
+import { backIn } from "./back"
 
-const createAnticipate = (power?: number): EasingFunction => {
-    const backEasing = createBackIn(power)
-    return (p) =>
-        (p *= 2) < 1
-            ? 0.5 * backEasing(p)
-            : 0.5 * (2 - Math.pow(2, -10 * (p - 1)))
-}
-
-export const anticipate = createAnticipate()
+export const anticipate = (p: number) =>
+    (p *= 2) < 1 ? 0.5 * backIn(p) : 0.5 * (2 - Math.pow(2, -10 * (p - 1)))
--- packages/framer-motion/src/easing/back.ts
+++ packages/framer-motion/src/easing/back.ts
@@ -1,12 +1,7 @@
+import { cubicBezier } from "./cubic-bezier"
 import { mirrorEasing } from "./modifiers/mirror"
 import { reverseEasing } from "./modifiers/reverse"
-import { EasingFunction } from "./types"
 
-export const createBackIn =
-    (power: number = 1.525): EasingFunction =>
-    (p) =>
-        p * p * ((power + 1) * p - power)
-
-export const backIn = createBackIn()
-export const backOut = reverseEasing(backIn)
+export const backOut = cubicBezier(0.33, 1.53, 0.69, 0.99)
+export const backIn = reverseEasing(backOut)
 export const backInOut = mirrorEasing(backIn)
