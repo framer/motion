d61cd90ff29ee4418a7aaefd58e8443a148eb944
Matt Perry <mattgperry@gmail.com>
2023-01-02 10:54:30 +0100

    Adding comment

--- packages/framer-motion/src/utils/use-motion-value-event.ts
+++ packages/framer-motion/src/utils/use-motion-value-event.ts
@@ -9,6 +9,12 @@ export function useMotionValueEvent<
     event: EventName,
     callback: MotionValueEventCallbacks<V>[EventName]
 ) {
+    /**
+     * useInsertionEffect will create subscriptions before any other
+     * effects will run. Effects run upwards through the tree so it
+     * can be that binding a useLayoutEffect higher up the tree can
+     * miss changes from lower down the tree.
+     */
     useInsertionEffect(
         () => value.on(event, callback),
         [value, event, callback]
