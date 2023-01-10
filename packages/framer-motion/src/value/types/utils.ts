9707614172eddcf92e7151e99d9b7995e5e7ebfb
Matt Perry <mattgperry@gmail.com>
2023-01-10 09:36:54 +0100

    Adding tests

--- packages/framer-motion/src/value/types/utils.ts
+++ packages/framer-motion/src/value/types/utils.ts
@@ -9,9 +9,9 @@ export const sanitize = (v: number) => Math.round(v * 100000) / 100000
 
 export const floatRegex = /(-)?([\d]*\.?[\d])+/g
 export const colorRegex =
-    /(#[0-9a-f]{2,8}|(rgb|hsl)a?\((-?[\d\.]+%?[,\s]+){2}(-?[\d\.]+%?)\s*[\,\/]?\s*[\d\.]*%?\))/gi
+    /(#[0-9a-f]{3,8}|(rgb|hsl)a?\((-?[\d\.]+%?[,\s]+){2}(-?[\d\.]+%?)\s*[\,\/]?\s*[\d\.]*%?\))/gi
 export const singleColorRegex =
-    /^(#[0-9a-f]{2,8}|(rgb|hsl)a?\((-?[\d\.]+%?[,\s]+){2}(-?[\d\.]+%?)\s*[\,\/]?\s*[\d\.]*%?\))$/i
+    /^(#[0-9a-f]{3,8}|(rgb|hsl)a?\((-?[\d\.]+%?[,\s]+){2}(-?[\d\.]+%?)\s*[\,\/]?\s*[\d\.]*%?\))$/i
 
 export function isString(v: any): v is string {
     return typeof v === "string"
