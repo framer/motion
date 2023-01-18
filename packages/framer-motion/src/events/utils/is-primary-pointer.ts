220c57682d253ef760863bb7770e1b7d02335581
Matt Perry <mattgperry@gmail.com>
2023-01-18 10:15:26 +0100

    Improving comments

--- packages/framer-motion/src/events/utils/is-primary-pointer.ts
+++ packages/framer-motion/src/events/utils/is-primary-pointer.ts
@@ -1,12 +1,15 @@
-/**
- * Specifically match against false here as incomplete versions of
- * PointerEvents in very old browser might have it set as undefined.
- */
 export const isPrimaryPointer = (event: PointerEvent) => {
-    console.log(event)
     if (event.pointerType === "mouse") {
         return typeof event.button !== "number" || event.button <= 0
     } else {
-        return event.isPrimary
+        /**
+         * isPrimary is true for all mice buttons, whereas every touch point
+         * is regarded as its own input. So subsequent concurrent touch points
+         * will be false.
+         *
+         * Specifically match against false here as incomplete versions of
+         * PointerEvents in very old browser might have it set as undefined.
+         */
+        return event.isPrimary !== false
     }
 }
