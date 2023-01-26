298ceb69f56d6fdb0fa2ef05e944efac23f0dd62
Matt Perry <mattgperry@gmail.com>
2023-01-26 11:40:27 +0100

    Fixing tests

--- packages/framer-motion/cypress/integration/appear.chrome.ts
+++ packages/framer-motion/cypress/integration/appear.chrome.ts
@@ -8,7 +8,7 @@ describe("Correctly plays and resumes from optimized appear animations", () => {
     tests.forEach((test) => {
         it(test, () => {
             cy.visit(test)
-            cy.wait(200)
+            cy.wait(1000)
                 .get('[data-layout-correct="false"]')
                 .should("not.exist")
         })
