1ff224360482e1af1b342be489c255e5fc65e919
Matt Perry <mattgperry@gmail.com>
2023-02-13 15:30:32 +0100

    Adding projection node dep

--- packages/framer-motion/src/motion/features/layout.ts
+++ packages/framer-motion/src/motion/features/layout.ts
@@ -1,8 +1,10 @@
+import { HTMLProjectionNode } from "../../projection/node/HTMLProjectionNode"
 import { MeasureLayout } from "./layout/MeasureLayout"
 import { FeaturePackages } from "./types"
 
 export const layout: FeaturePackages = {
     layout: {
+        ProjectionNode: HTMLProjectionNode,
         MeasureLayout,
     },
 }
