b465cb15d39268b7eee533ee8cc81ba3782e96e8
Matt Perry <mattgperry@gmail.com>
2023-02-13 14:10:44 +0100

    reducing size of focus functions

--- packages/framer-motion/src/gestures/focus.ts
+++ packages/framer-motion/src/gestures/focus.ts
@@ -9,37 +9,37 @@ export class FocusGesture extends Feature<Element> {
 
     private removeEventListeners: Function = noop
 
+    onFocus() {
+        let isFocusVisible = false
+
+        /**
+         * If this element doesn't match focus-visible then don't
+         * apply whileHover. But, if matches throws that focus-visible
+         * is not a valid selector then in that browser outline styles will be applied
+         * to the element by default and we want to match that behaviour with whileFocus.
+         */
+        try {
+            isFocusVisible = this.node.current!.matches(":focus-visible")
+        } catch (e) {
+            isFocusVisible = true
+        }
+
+        if (!isFocusVisible || !this.node.animationState) return
+
+        this.node.animationState.setActive(AnimationType.Focus, true)
+        this.isActive = true
+    }
+
+    onBlur() {
+        if (!this.isActive || !this.node.animationState) return
+        this.node.animationState.setActive(AnimationType.Focus, false)
+        this.isActive = false
+    }
+
     mount() {
-        const onFocus = () => {
-            let isFocusVisible = false
-
-            /**
-             * If this element doesn't match focus-visible then don't
-             * apply whileHover. But, if matches throws that focus-visible
-             * is not a valid selector then in that browser outline styles will be applied
-             * to the element by default and we want to match that behaviour with whileFocus.
-             */
-            try {
-                isFocusVisible = this.node.current!.matches(":focus-visible")
-            } catch (e) {
-                isFocusVisible = true
-            }
-
-            if (!isFocusVisible || !this.node.animationState) return
-
-            this.node.animationState.setActive(AnimationType.Focus, true)
-            this.isActive = true
-        }
-
-        const onBlur = () => {
-            if (!this.isActive || !this.node.animationState) return
-            this.node.animationState.setActive(AnimationType.Focus, false)
-            this.isActive = false
-        }
-
         this.removeEventListeners = pipe(
-            addDomEvent(this.node.current!, "focus", onFocus),
-            addDomEvent(this.node.current!, "blur", onBlur)
+            addDomEvent(this.node.current!, "focus", () => this.onFocus()),
+            addDomEvent(this.node.current!, "blur", () => this.onBlur())
         )
     }
 
