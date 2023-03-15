8d83f16a67f94e096f7364f406556a73ffdd009c
Matt Perry <mattgperry@gmail.com>
2023-03-15 13:14:07 +0100

    Adding types

---
+++ packages/framer-motion/src/animation/hooks/__tests__/use-animate.test.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { useEffect } from "react"
+import { render } from "@testing-library/react"
+import { useAnimate } from "../use-animate"
+
+describe("useAnimate", () => {
+    test("Types work as expected", () => {
+        const Component = () => {
+            const [scope, animate] = useAnimate()
+
+            useEffect(() => {
+                animate("div", { opacity: 1 })
+            })
+
+            return <div ref={scope} />
+        }
+        render(<Component />)
+    })
+})
