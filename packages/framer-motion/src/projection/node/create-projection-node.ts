ab9276c46cb87b24404098090d8bd40c0f705ed9
Matt Perry <mattgperry@gmail.com>
2023-04-26 11:43:39 +0200

    Attempting fix for jumpy layers

--- packages/framer-motion/src/projection/node/create-projection-node.ts
+++ packages/framer-motion/src/projection/node/create-projection-node.ts
@@ -1053,7 +1053,7 @@ export function createProjectionNode<I>({
             if (!this.targetDelta && !this.relativeTarget) {
                 // TODO: This is a semi-repetition of further down this function, make DRY
                 const relativeParent = this.getClosestProjectingParent()
-                if (relativeParent && relativeParent.layout) {
+                if (relativeParent && relativeParent.layout && this.animationProgress !== 1) {
                     this.relativeParent = relativeParent
                     this.forceRelativeParentToResolveTarget()
                     this.relativeTarget = createBox()
@@ -1132,7 +1132,7 @@ export function createProjectionNode<I>({
                     Boolean(relativeParent.resumingFrom) ===
                         Boolean(this.resumingFrom) &&
                     !relativeParent.options.layoutScroll &&
-                    relativeParent.target
+                    relativeParent.target && this.animationProgress !== 1
                 ) {
                     this.relativeParent = relativeParent
                     this.forceRelativeParentToResolveTarget()
@@ -1431,6 +1431,7 @@ export function createProjectionNode<I>({
                 this.animationProgress = progress
             }
 
+            if ()
             this.mixTargetDelta(this.options.layoutRoot ? 1000 : 0)
         }
 
