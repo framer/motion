c9d50f2b86409110905aee9c950ea01b1ee73e73
Matt Perry <mattgperry@gmail.com>
2023-07-25 16:16:15 +0200

    Updating effect

--- packages/framer-motion/src/motion/utils/use-visual-element.ts
+++ packages/framer-motion/src/motion/utils/use-visual-element.ts
@@ -66,7 +66,7 @@ export function useVisualElement<Instance, RenderState>(
      * are running, we use useLayoutEffect to trigger animations.
      */
     const useAnimateChangesEffect = window.HandoffAppearAnimations
-        ? useEffect
+        ? useIsomorphicLayoutEffect
         : useEffect
     useAnimateChangesEffect(() => {
         if (visualElement && visualElement.animationState) {
