850a13178f224ce05e8aa3600f31cf15e450dd8a
Matt Perry <mattgperry@gmail.com>
2023-09-04 17:07:17 +0200

    Latest

---
+++ packages/framer-motion/src/animation/animators/AsyncValueAnimation.ts
@@ -0,0 +1,11 @@
+import { ValueAnimation } from "./ValueAnimation"
+
+export class AsyncValueAnimation extends ValueAnimation {
+    constructor() {
+        super()
+        /**
+         * 1. Make animatable
+         *    - We only need to resolve "auto"
+         */
+    }
+}
---
+++ packages/framer-motion/src/animation/animators/SyncValueAnimation.ts
@@ -0,0 +1,12 @@
+import { ValueAnimation } from "./ValueAnimation"
+
+export class SyncValueAnimation extends ValueAnimation {
+    constructor() {
+        super()
+        /**
+         * 1. Make animatable
+         *    - If this is changing value types, or reading CSS values
+         *      we need to read those
+         */
+    }
+}
