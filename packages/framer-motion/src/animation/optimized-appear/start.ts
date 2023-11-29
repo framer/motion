5a2c2ef5565111030ae762c9c5cf18a4203e8d31
Matt Perry <mattgperry@gmail.com>
2023-11-29 12:02:46 +0100

    Ensure optimised animations don't run after hydration

--- packages/framer-motion/src/animation/optimized-appear/start.ts
+++ packages/framer-motion/src/animation/optimized-appear/start.ts
@@ -13,6 +13,9 @@ export function startOptimizedAppearAnimation(
     options: NativeAnimationOptions,
     onReady?: (animation: Animation) => void
 ): void {
+    // Prevent optimised appear animations if Motion has already started animating.
+    if (window.HandoffAppearAnimations === false) return
+
     const id = element.dataset[optimizedAppearDataId]
 
     if (!id) return
