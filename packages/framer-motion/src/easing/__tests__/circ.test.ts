db1af26a752f1c30ab6566085acc2d224857c4fc
Matt Perry <mattgperry@gmail.com>
2023-12-08 16:32:47 +0100

    Updating test

--- packages/framer-motion/src/easing/__tests__/circ.test.ts
+++ packages/framer-motion/src/easing/__tests__/circ.test.ts
@@ -2,7 +2,7 @@ import { circIn, circOut, circInOut } from "../circ"
 
 describe("circ easing", () => {
     test("circInOut is correct", () => {
-        expect(circInOut(0.25)).toBe(circIn(0.5))
-        expect(circInOut(0.75)).toBe(circOut(0.5))
+        expect(circInOut(0.25)).toBe(circIn(0.5) / 2)
+        expect(circInOut(0.75)).toBe(0.5 + circOut(0.5) / 2)
     })
 })
