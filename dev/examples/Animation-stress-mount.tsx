7e2dd9944dd519e06b09e1729f888813dddc7feb
Matt Perry <mattgperry@gmail.com>
2023-12-15 09:27:45 +0100

    Adding stress test

---
+++ dev/examples/Animation-stress-mount.tsx
@@ -0,0 +1,71 @@
+import * as React from "react"
+import { motion } from "framer-motion"
+
+/**
+ * An example of the tween transition type
+ */
+
+const style = {
+    width: 100,
+    height: 100,
+    background: "white",
+}
+const Box = () => {
+    return (
+        <motion.div
+            animate={{ backgroundColor: "#f00", rotate: 90, opacity: [0, 1] }}
+            style={style}
+        />
+    )
+}
+
+export const App = () => {
+    return (
+        <div style={{ width: 1000, display: "flex", flexWrap: "wrap" }}>
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+        </div>
+    )
+}
