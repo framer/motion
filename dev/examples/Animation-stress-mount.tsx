fb5e9853e37e2da39830105a758378d344c59b53
Matt Perry <mattgperry@gmail.com>
2023-12-15 15:01:11 +0100

    Updating stress test

--- dev/examples/Animation-stress-mount.tsx
+++ dev/examples/Animation-stress-mount.tsx
@@ -5,17 +5,29 @@ import { motion } from "framer-motion"
  * An example of the tween transition type
  *
  * Effect time at 6x speed, ~120ms
+ * Lower bound when no animations successfully fire - 9ms
  */
 
 const style = {
     width: 100,
     height: 100,
     background: "white",
+    "--number": 0,
 }
 const Box = () => {
     return (
         <motion.div
-            animate={{ backgroundColor: "#f00", rotate: 90, opacity: [0, 1] }}
+            animate={{
+                backgroundColor: "var(--color)",
+                rotate: 90,
+                opacity: [0, 1],
+                x: "50%",
+                y: "50%",
+                filter: [null, "blur(1px)"],
+                "--number": 1,
+            }}
+            initial={{ x: 1, y: 0 }}
+            transition={{ duration: 1 }}
             style={style}
         />
     )
@@ -23,7 +35,14 @@ const Box = () => {
 
 export const App = () => {
     return (
-        <div style={{ width: 1000, display: "flex", flexWrap: "wrap" }}>
+        <div
+            style={{
+                width: 1000,
+                display: "flex",
+                flexWrap: "wrap",
+                "--color": "#f00",
+            }}
+        >
             <Box />
             <Box />
             <Box />
