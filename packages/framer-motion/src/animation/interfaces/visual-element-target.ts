0e6e33ff9678535389827435bf8222bdb2204d20
Matt Perry <mattgperry@gmail.com>
2024-01-12 15:36:45 +0100

    Latest

--- packages/framer-motion/src/animation/interfaces/visual-element-target.ts
+++ packages/framer-motion/src/animation/interfaces/visual-element-target.ts
@@ -144,6 +144,7 @@ export function animateTarget(
 
     if (transitionEnd) {
         Promise.all(animations).then(() => {
+            console.log("transitionEnd", transitionEnd)
             transitionEnd && setTarget(visualElement, transitionEnd)
         })
     }
