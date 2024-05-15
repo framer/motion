1382f1d727d1f2d165518a3412390b4e290006db
Matt Perry <mattgperry@gmail.com>
2024-05-15 16:24:02 +0200

    Updating useSpring demo

--- dev/examples/useSpring.tsx
+++ dev/examples/useSpring.tsx
@@ -1,10 +1,35 @@
-import { motion, useMotionValue, useSpring } from "framer-motion"
+import { frame, motion, useMotionValue, useSpring } from "framer-motion"
+import { useEffect, useRef, useState } from "react"
+
+const spring = {
+    stiffness: 300,
+    damping: 28,
+    restDelta: 0.001,
+    restSpeed: 0.001,
+}
+
+const useMousePosition = () => {
+    const [mousePosition, setMousePosition] = useState({ x: null, y: null })
+
+    const updateMousePosition = (e) => {
+        setMousePosition({ x: e.clientX, y: e.clientY })
+    }
+
+    useEffect(() => {
+        window.addEventListener("mousemove", updateMousePosition)
+
+        return () =>
+            window.removeEventListener("mousemove", updateMousePosition)
+    }, [])
+
+    return mousePosition
+}
 
 function DragExample() {
     const dragX = useMotionValue(0)
     const dragY = useMotionValue(0)
-    const x = useSpring(dragX, { stiffness: 300, damping: 28 })
-    const y = useSpring(dragY, { stiffness: 300, damping: 28 })
+    const x = useSpring(dragX)
+    const y = useSpring(dragY, spring)
     return (
         <motion.div
             drag
@@ -12,7 +37,89 @@ function DragExample() {
             _dragX={dragX}
             _dragY={dragY}
             style={{ width: 100, height: 100, background: "red", x, y }}
-        />
+        >
+            Drag
+        </motion.div>
+    )
+}
+
+function RerenderExample() {
+    const { x, y } = useMousePosition()
+    const size = 40
+    const ref = useRef<HTMLDivElement>(null)
+    const onMove = useRef<(event: MouseEvent) => void>(
+        ({ clientX, clientY }: MouseEvent) => {
+            const element = ref.current!
+
+            setPoint({
+                x: clientX - element.offsetLeft - element.offsetWidth / 2,
+                y: clientY - element.offsetTop - element.offsetHeight / 2,
+            })
+        }
+    )
+
+    function startPointer() {
+        window.addEventListener("pointermove", onMove.current)
+    }
+
+    function cancelPointer() {
+        window.removeEventListener("pointermove", onMove.current)
+    }
+
+    return (
+        <motion.div
+            ref={ref}
+            animate={{ x }}
+            transition={spring}
+            style={{
+                width: 100,
+                height: 100,
+                background: "green",
+                position: "absolute",
+                inset: 0,
+            }}
+            onTapStart={startPointer}
+            onTapCancel={cancelPointer}
+            onTap={cancelPointer}
+        >
+            Rerender
+        </motion.div>
+    )
+}
+
+function MouseEventExample() {
+    const xPoint = useMotionValue(0)
+    const yPoint = useMotionValue(0)
+    const x = useSpring(xPoint, spring)
+    const y = useSpring(yPoint, spring)
+    const ref = useRef<HTMLDivElement>(null)
+    const onMove = useRef<(event: MouseEvent) => void>(
+        ({ clientX, clientY }: MouseEvent) => {
+            const element = ref.current!
+
+            xPoint.set(clientX - element.offsetLeft - element.offsetWidth / 2)
+            yPoint.set(clientY - element.offsetTop - element.offsetHeight / 2)
+        }
+    )
+
+    function startPointer() {
+        window.addEventListener("pointermove", onMove.current)
+    }
+
+    function cancelPointer() {
+        window.removeEventListener("pointermove", onMove.current)
+    }
+
+    return (
+        <motion.div
+            ref={ref}
+            style={{ width: 100, height: 100, background: "yellow", x, y }}
+            onTapStart={startPointer}
+            onTapCancel={cancelPointer}
+            onTap={cancelPointer}
+        >
+            Mouse Event
+        </motion.div>
     )
 }
 
@@ -53,7 +160,9 @@ export function App() {
     return (
         <div style={{ display: "flex", gap: 100 }}>
             <DragExample />
-            <AnimationExample />
+            <MouseEventExample />
+            <RerenderExample />
+            {/* <AnimationExample /> */}
         </div>
     )
 }
