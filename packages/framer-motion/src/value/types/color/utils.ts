c009b6aff10d98b3f5652f881a197c61606d511f
Jacob Groß <kurtextrem@gmail.com>
2024-06-18 12:38:32 +0200

    fix(isColorString): cannot convert undefined or null to object

--- packages/framer-motion/src/value/types/color/utils.ts
+++ packages/framer-motion/src/value/types/color/utils.ts
@@ -8,7 +8,7 @@ import { floatRegex, isString, singleColorRegex } from "../utils"
 export const isColorString = (type: string, testProp?: string) => (v: any) => {
     return Boolean(
         (isString(v) && singleColorRegex.test(v) && v.startsWith(type)) ||
-            (testProp && Object.prototype.hasOwnProperty.call(v, testProp))
+            (testProp && !isNullish(v) && Object.prototype.hasOwnProperty.call(v, testProp))
     )
 }
 
