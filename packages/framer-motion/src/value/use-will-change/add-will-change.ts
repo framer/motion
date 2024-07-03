6b918561bb77bc574e3029e01170b806c5ff7588
Matt Perry <mattgperry@gmail.com>
2024-07-03 12:26:40 +0200

    Improving comments

--- packages/framer-motion/src/value/use-will-change/add-will-change.ts
+++ packages/framer-motion/src/value/use-will-change/add-will-change.ts
@@ -1,22 +1,29 @@
 import { WillChangeMotionValue } from "."
 import type { VisualElement } from "../../render/VisualElement"
+import { isWillChangeMotionValue } from "./is"
 
 export function addValueToWillChange(
     visualElement: VisualElement,
     key: string
 ) {
-    if (!visualElement.applyWillChange) {
-        return
-    }
+    if (!visualElement.applyWillChange) return
 
-    let willChange = visualElement.getValue(
-        "willChange"
-    ) as WillChangeMotionValue
+    let willChange = visualElement.getValue("willChange")
 
+    /**
+     * If we haven't created a willChange MotionValue, and the we haven't been
+     * manually provided one, create one.
+     */
     if (!willChange && !visualElement.props.style?.willChange) {
         willChange = new WillChangeMotionValue("auto")
         visualElement.addValue("willChange", willChange)
     }
 
-    return willChange.add(key)
+    /**
+     * It could be that a user has set willChange to a regular MotionValue,
+     * in which case we can't add the value to it.
+     */
+    if (isWillChangeMotionValue(willChange)) {
+        return willChange.add(key)
+    }
 }
