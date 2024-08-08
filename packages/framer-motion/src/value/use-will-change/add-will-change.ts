00edfc06787c8c0f5d136bca3d865569acbde705
Matt Perry <mattgperry@gmail.com>
2024-08-08 10:49:22 +0200

    Disabling auto will change

--- packages/framer-motion/src/value/use-will-change/add-will-change.ts
+++ packages/framer-motion/src/value/use-will-change/add-will-change.ts
@@ -1,4 +1,4 @@
-import { WillChangeMotionValue } from "./WillChangeMotionValue"
+// import { WillChangeMotionValue } from "./WillChangeMotionValue"
 import type { VisualElement } from "../../render/VisualElement"
 import { isWillChangeMotionValue } from "./is"
 
@@ -8,16 +8,16 @@ export function addValueToWillChange(
 ) {
     if (!visualElement.applyWillChange) return
 
-    let willChange = visualElement.getValue("willChange")
+    const willChange = visualElement.getValue("willChange")
 
     /**
      * If we haven't created a willChange MotionValue, and the we haven't been
      * manually provided one, create one.
      */
-    if (!willChange && !visualElement.props.style?.willChange) {
-        willChange = new WillChangeMotionValue("auto")
-        visualElement.addValue("willChange", willChange)
-    }
+    // if (!willChange && !visualElement.props.style?.willChange) {
+    //     willChange = new WillChangeMotionValue("auto")
+    //     visualElement.addValue("willChange", willChange)
+    // }
 
     /**
      * It could be that a user has set willChange to a regular MotionValue,
