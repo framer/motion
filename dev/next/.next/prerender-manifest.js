e3d872c041363ffe600c84e96b2d64745d135530
Matt Perry <mattgperry@gmail.com>
2024-09-03 09:16:22 +0200

    Removing .next

--- dev/next/.next/prerender-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__PRERENDER_MANIFEST="{\"preview\":{\"previewModeId\":\"process.env.__NEXT_PREVIEW_MODE_ID\",\"previewModeSigningKey\":\"process.env.__NEXT_PREVIEW_MODE_SIGNING_KEY\",\"previewModeEncryptionKey\":\"process.env.__NEXT_PREVIEW_MODE_ENCRYPTION_KEY\"}}"
--- dev/next/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"pqWDQr4btszyBd6sUxhHt9JSmhpDHEQDOogAMLGoF44=\"}"
