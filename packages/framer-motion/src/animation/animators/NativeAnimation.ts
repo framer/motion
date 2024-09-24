d3c127ba10a4e5245983d8e8a7982c6adecc4493
Matt Perry <mattgperry@gmail.com>
2024-09-24 11:35:14 +0200

    Latest

---
+++ packages/framer-motion/src/animation/animators/NativeAnimation.ts
@@ -0,0 +1,187 @@
+import { memo } from "../../utils/memo"
+import {
+    AnimationPlaybackControls,
+    ValueAnimationOptions,
+    ValueKeyframesDefinition,
+} from "../types"
+
+const supportsWaapi = /*@__PURE__*/ memo(() =>
+    Object.hasOwnProperty.call(Element.prototype, "animate")
+)
+
+export class NativeAnimation implements AnimationPlaybackControls {
+    resolved: ResolvedAcceleratedAnimation
+    constructor(
+        element: Element,
+        valueName: string,
+        valueKeyframes: ValueKeyframesDefinition,
+        valueOptions: ValueAnimationOptions
+    ) {}
+    get duration() {
+        const { resolved } = this
+        if (!resolved) return 0
+        const { duration } = resolved
+        return millisecondsToSeconds(duration)
+    }
+
+    get time() {
+        const { resolved } = this
+        if (!resolved) return 0
+        const { animation } = resolved
+        return millisecondsToSeconds((animation.currentTime as number) || 0)
+    }
+
+    set time(newTime: number) {
+        const { resolved } = this
+        if (!resolved) return
+
+        const { animation } = resolved
+        animation.currentTime = secondsToMilliseconds(newTime)
+    }
+
+    get speed() {
+        const { resolved } = this
+        if (!resolved) return 1
+
+        const { animation } = resolved
+        return animation.playbackRate
+    }
+
+    set speed(newSpeed: number) {
+        const { resolved } = this
+        if (!resolved) return
+
+        const { animation } = resolved
+        animation.playbackRate = newSpeed
+    }
+
+    get state() {
+        const { resolved } = this
+        if (!resolved) return "idle"
+
+        const { animation } = resolved
+        return animation.playState
+    }
+
+    get startTime() {
+        const { resolved } = this
+        if (!resolved) return null
+
+        const { animation } = resolved
+
+        // Coerce to number as TypeScript incorrectly types this
+        // as CSSNumberish
+        return animation.startTime as number
+    }
+
+    /**
+     * Replace the default DocumentTimeline with another AnimationTimeline.
+     * Currently used for scroll animations.
+     */
+    attachTimeline(timeline: any) {
+        if (!this._resolved) {
+            this.pendingTimeline = timeline
+        } else {
+            const { resolved } = this
+            if (!resolved) return noop<void>
+
+            const { animation } = resolved
+
+            animation.timeline = timeline
+            animation.onfinish = null
+        }
+
+        return noop<void>
+    }
+
+    play() {
+        if (this.isStopped) return
+        const { resolved } = this
+        if (!resolved) return
+
+        const { animation } = resolved
+
+        if (animation.playState === "finished") {
+            this.updateFinishedPromise()
+        }
+
+        animation.play()
+    }
+
+    pause() {
+        const { resolved } = this
+        if (!resolved) return
+
+        const { animation } = resolved
+        animation.pause()
+    }
+
+    stop() {
+        this.resolver.cancel()
+        this.isStopped = true
+        if (this.state === "idle") return
+
+        this.resolveFinishedPromise()
+        this.updateFinishedPromise()
+
+        const { resolved } = this
+        if (!resolved) return
+
+        const { animation, keyframes, duration, type, ease, times } = resolved
+
+        if (
+            animation.playState === "idle" ||
+            animation.playState === "finished"
+        ) {
+            return
+        }
+
+        /**
+         * WAAPI doesn't natively have any interruption capabilities.
+         *
+         * Rather than read commited styles back out of the DOM, we can
+         * create a renderless JS animation and sample it twice to calculate
+         * its current value, "previous" value, and therefore allow
+         * Motion to calculate velocity for any subsequent animation.
+         */
+        if (this.time) {
+            const { motionValue, onUpdate, onComplete, element, ...options } =
+                this.options
+
+            // const sampleAnimation = new MainThreadAnimation({
+            //     ...options,
+            //     keyframes,
+            //     duration,
+            //     type,
+            //     ease,
+            //     times,
+            //     isGenerator: true,
+            // })
+
+            const sampleTime = secondsToMilliseconds(this.time)
+
+            motionValue.setWithVelocity(
+                sampleAnimation.sample(sampleTime - sampleDelta).value,
+                sampleAnimation.sample(sampleTime).value,
+                sampleDelta
+            )
+        }
+
+        const { onStop } = this.options
+        onStop && onStop()
+
+        this.cancel()
+    }
+
+    complete() {
+        const { resolved } = this
+        if (!resolved) return
+        resolved.animation.finish()
+    }
+
+    cancel() {
+        const { resolved } = this
+        if (!resolved) return
+        resolved.animation.cancel()
+    }
+}
--- packages/framer-motion/src/animation/animators/waapi/animate-dom.ts
+++ packages/framer-motion/src/animation/animators/waapi/animate-dom.ts
@@ -1,14 +1,88 @@
+import { resolveElements } from "../../../render/dom/utils/resolve-element"
+import { invariant } from "../../../utils/errors"
+import { secondsToMilliseconds } from "../../../utils/time-conversion"
+import { GroupPlaybackControls } from "../../GroupPlaybackControls"
 import {
+    AnimationPlaybackControls,
     AnimationScope,
     DOMKeyframesDefinition,
     DynamicAnimationOptions,
     ElementOrSelector,
+    Transition,
 } from "../../types"
+import { getValueTransition } from "../../utils/transitions"
+import { NativeAnimation } from "../NativeAnimation"
 
-export const createScopedWaapiAnimate = (_scope?: AnimationScope) => {
-    return (
-        _value: ElementOrSelector,
-        _keyframes: DOMKeyframesDefinition,
-        _options?: DynamicAnimationOptions
-    ) => {}
+function animateElement(
+    element: Element,
+    keyframes: DOMKeyframesDefinition,
+    options: Transition
+): AnimationPlaybackControls[] {
+    const animations: AnimationPlaybackControls[] = []
+
+    for (const valueName in keyframes) {
+        const valueKeyframes = keyframes[valueName as keyof typeof keyframes]!
+        const valueOptions = getValueTransition(options as any, valueName)
+
+        if (options.duration) {
+            options.duration = secondsToMilliseconds(options.duration)
+        }
+        if (options.delay) {
+            options.delay = secondsToMilliseconds(options.delay || 0)
+        }
+
+        animations.push(
+            new NativeAnimation(
+                element,
+                valueName,
+                valueKeyframes,
+                valueOptions
+            )
+        )
+    }
+
+    return animations
 }
+
+export const createScopedWaapiAnimate = (scope?: AnimationScope) => {
+    // TODO: This could be combined with the function in animation/animate
+    return (
+        elementOrSelector: ElementOrSelector,
+        keyframes: DOMKeyframesDefinition,
+        options?: DynamicAnimationOptions
+    ): AnimationPlaybackControls => {
+        const elements = resolveElements(elementOrSelector, scope)
+        const numElements = elements.length
+
+        invariant(Boolean(numElements), "No valid element provided.")
+
+        const animations: AnimationPlaybackControls[] = []
+
+        for (let i = 0; i < numElements; i++) {
+            const element = elements[i]
+            const elementTransition = { ...options }
+
+            /**
+             * Resolve stagger function if provided.
+             */
+            if (typeof elementTransition.delay === "function") {
+                elementTransition.delay = elementTransition.delay(
+                    i,
+                    numElements
+                )
+            }
+
+            animations.push(
+                ...animateElement(
+                    element,
+                    keyframes,
+                    elementTransition as Transition
+                )
+            )
+        }
+
+        return new GroupPlaybackControls(animations)
+    }
+}
+
+export const animateDom = createScopedWaapiAnimate()
