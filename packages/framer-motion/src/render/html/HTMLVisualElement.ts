a2eec07cb84a8bb6185449f170f3f1f484e71dd2
Simon Karman <simon.karman@xebia.com>
2024-10-23 21:44:02 +0200

    Still use textContent where possible

--- packages/framer-motion/src/render/html/HTMLVisualElement.ts
+++ packages/framer-motion/src/render/html/HTMLVisualElement.ts
@@ -81,9 +81,16 @@ export class HTMLVisualElement extends DOMVisualElement<
             this.childSubscription = children.on("change", (latest) => {
                 if (
                     this.current
+                    && this.current.textContent
+                ) {
+                    // In H1, H2, etc. the textContent can be set directly
+                    this.current.textContent = `${latest}`
+                } else if (
+                    this.current
                     && this.current.firstChild
                     && this.current.firstChild.nodeType === Node.TEXT_NODE
                 ) {
+                    // In SVG, the text can only be set through the value of the child text node
                     this.current.firstChild.nodeValue = `${latest}`
                 }
             })
