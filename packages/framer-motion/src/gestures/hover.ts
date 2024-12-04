d385f5597ccefb2dd5dd1239ae938b065132ce45
Matt Perry <mattgperry@gmail.com>
2024-12-04 10:32:30 +0100

    Always use passive events

--- packages/framer-motion/src/gestures/hover.ts
+++ packages/framer-motion/src/gestures/hover.ts
@@ -3,19 +3,20 @@ import { Feature } from "../motion/features/Feature"
 import { frame } from "../frameloop"
 import { hover } from "motion-dom"
 import { extractEventInfo } from "../events/event-info"
+import { MotionProps } from "../motion/types"
 
 function handleHoverEvent(
     node: VisualElement<Element>,
     event: PointerEvent,
-    isActive: boolean
+    lifecycle: "Start" | "End"
 ) {
     const { props } = node
 
     if (node.animationState && props.whileHover) {
-        node.animationState.setActive("whileHover", isActive)
+        node.animationState.setActive("whileHover", lifecycle === "Start")
     }
 
-    const callback = props[isActive ? "onHoverStart" : "onHoverEnd"]
+    const callback = props[("onHover" + lifecycle) as keyof MotionProps]
     if (callback) {
         frame.postRender(() => callback(event, extractEventInfo(event)))
     }
@@ -23,19 +24,14 @@ function handleHoverEvent(
 
 export class HoverGesture extends Feature<Element> {
     mount() {
-        const { current, props } = this.node
+        const { current } = this.node
         if (!current) return
 
-        this.unmount = hover(
-            current,
-            (startEvent) => {
-                handleHoverEvent(this.node, startEvent, true)
+        this.unmount = hover(current, (startEvent) => {
+            handleHoverEvent(this.node, startEvent, "Start")
 
-                return (endEvent) =>
-                    handleHoverEvent(this.node, endEvent, false)
-            },
-            { passive: !props.onHoverStart && !props.onHoverEnd }
-        )
+            return (endEvent) => handleHoverEvent(this.node, endEvent, "End")
+        })
     }
 
     unmount() {}
